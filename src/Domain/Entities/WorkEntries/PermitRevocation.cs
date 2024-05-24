75a658090aa2092e6594b9ceaa534bd07d2dbb37
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-05-24 08:25:10 -0400

    Add Permit Revocation entity

---
+++ src/Domain/Entities/WorkEntries/PermitRevocation.cs
@@ -0,0 +1,11 @@
+﻿namespace AirWeb.Domain.Entities.WorkEntries;
+
+public class PermitRevocation : BaseWorkEntry
+{
+    internal PermitRevocation(int? id) : base(id) => WorkEntryType = WorkEntryType.PermitRevocation;
+
+    public DateOnly ReceivedDate { get; init; }
+    public DateOnly PermitRevocationDate { get; init; }
+    public DateOnly? PhysicalShutdownDate { get; init; }
+    public bool FollowupTaken { get; init; }
+}
