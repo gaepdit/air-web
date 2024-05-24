ab933f1fb56d0f44f33e1c5759aac14c710c25bd
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-05-24 08:41:30 -0400

    Add ACC entity

---
+++ src/Domain/Entities/WorkEntries/AnnualComplianceCertification.cs
@@ -0,0 +1,21 @@
+﻿namespace AirWeb.Domain.Entities.WorkEntries;
+
+public class AnnualComplianceCertification : BaseWorkEntry
+{
+    internal AnnualComplianceCertification(int? id) : base(id) =>
+        WorkEntryType = WorkEntryType.AnnualComplianceCertification;
+
+    public DateOnly ReceivedDate { get; init; }
+    public int AccReportingYear { get; init; }
+    public DateOnly PostmarkedDate { get; init; }
+    public bool PostmarkedOnTime { get; init; }
+    public bool SignedByRo { get; init; }
+    public bool OnCorrectForms { get; init; }
+    public bool IncludesAllTvConditions { get; init; }
+    public bool CorrectlyCompleted { get; init; }
+    public bool ReportsDeviations { get; init; }
+    public bool IncludesPreviouslyUnreportedDeviations { get; init; }
+    public bool ReportsAllKnownDeviations { get; init; }
+    public bool ResubmittalRequired { get; init; }
+    public bool EnforcementNeeded { get; init; }
+}
