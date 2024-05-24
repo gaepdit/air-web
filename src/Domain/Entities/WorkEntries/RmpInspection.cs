49d29f25cafa6407786fe94f451d70f18b86feb3
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-05-24 08:40:46 -0400

    Add RMP Inspection entity

---
+++ src/Domain/Entities/WorkEntries/RmpInspection.cs
@@ -0,0 +1,9 @@
+﻿namespace AirWeb.Domain.Entities.WorkEntries;
+
+public class RmpInspection : BaseWorkEntry
+{
+    internal RmpInspection(int? id) : base(id) => WorkEntryType = WorkEntryType.RmpInspection;
+
+    public InspectionReason? InspectionReason { get; init; }
+    public ComplianceStatus ComplianceStatus { get; init; }
+}
