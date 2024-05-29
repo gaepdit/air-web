c752ce11d135039f40ed082468442a78ef71af1b
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-05-29 15:22:34 -0400

    Update AutoMapper profile

--- src/AppServices/AutoMapper/AutoMapperProfile.cs
+++ src/AppServices/AutoMapper/AutoMapperProfile.cs
@@ -1,15 +1,20 @@
-﻿using AutoMapper;
-using AirWeb.AppServices.EntryActions.Dto;
+﻿using AirWeb.AppServices.EntryActions.Dto;
 using AirWeb.AppServices.EntryTypes;
 using AirWeb.AppServices.Offices;
 using AirWeb.AppServices.Staff.Dto;
-using AirWeb.AppServices.WorkEntries.BaseWorkEntryDto;
-using AirWeb.AppServices.WorkEntries.Search;
+using AirWeb.AppServices.WorkEntries.Accs;
+using AirWeb.AppServices.WorkEntries.Inspections;
+using AirWeb.AppServices.WorkEntries.Notifications;
+using AirWeb.AppServices.WorkEntries.PermitRevocations;
+using AirWeb.AppServices.WorkEntries.Reports;
+using AirWeb.AppServices.WorkEntries.RmpInspections;
+using AirWeb.AppServices.WorkEntries.SourceTestReviews;
 using AirWeb.Domain.Entities.EntryActions;
 using AirWeb.Domain.Entities.EntryTypes;
 using AirWeb.Domain.Entities.Offices;
 using AirWeb.Domain.Entities.WorkEntries;
 using AirWeb.Domain.Identity;
+using AutoMapper;
 
 namespace AirWeb.AppServices.AutoMapper;
 
@@ -29,8 +34,33 @@ public class AutoMapperProfile : Profile
         CreateMap<Office, OfficeUpdateDto>();
         CreateMap<Office, OfficeViewDto>();
 
-        CreateMap<BaseWorkEntry, WorkEntrySearchResultDto>();
-        CreateMap<BaseWorkEntry, BaseWorkEntryCreateDto>();
-        CreateMap<BaseWorkEntry, BaseWorkEntryViewDto>();
+        // Work entries
+        CreateMap<AnnualComplianceCertification, AccCreateDto>();
+        CreateMap<AnnualComplianceCertification, AccUpdateDto>();
+        CreateMap<AnnualComplianceCertification, AccViewDto>();
+
+        CreateMap<Inspection, InspectionCreateDto>();
+        CreateMap<Inspection, InspectionUpdateDto>();
+        CreateMap<Inspection, InspectionViewDto>();
+
+        CreateMap<Notification, NotificationCreateDto>();
+        CreateMap<Notification, NotificationUpdateDto>();
+        CreateMap<Notification, NotificationViewDto>();
+
+        CreateMap<PermitRevocation, PermitRevocationCreateDto>();
+        CreateMap<PermitRevocation, PermitRevocationUpdateDto>();
+        CreateMap<PermitRevocation, PermitRevocationViewDto>();
+
+        CreateMap<Report, ReportCreateDto>();
+        CreateMap<Report, ReportUpdateDto>();
+        CreateMap<Report, ReportViewDto>();
+
+        CreateMap<RmpInspection, RmpInspectionCreateDto>();
+        CreateMap<RmpInspection, RmpInspectionUpdateDto>();
+        CreateMap<RmpInspection, RmpInspectionViewDto>();
+
+        CreateMap<SourceTestReview, SourceTestReviewCreateDto>();
+        CreateMap<SourceTestReview, SourceTestReviewUpdateDto>();
+        CreateMap<SourceTestReview, SourceTestReviewViewDto>();
     }
 }
