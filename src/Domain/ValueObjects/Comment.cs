cc5ef39fa24fc0af89e5687f1e749680456e5085
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-05-29 08:24:31 -0400

    Add date to comment value object

--- src/Domain/ValueObjects/Comment.cs
+++ src/Domain/ValueObjects/Comment.cs
@@ -6,10 +6,16 @@ namespace AirWeb.Domain.ValueObjects;
 [Owned]
 public record Comment : ValueObject
 {
-    public int Id { get; init; }
+    public Guid Id { get; init; }
 
     [StringLength(7000)]
     public string Text { get; [UsedImplicitly] init; } = string.Empty;
 
-    protected override IEnumerable<object> GetEqualityComponents() => new List<object> { Text }.AsReadOnly();
+    public DateTimeOffset CommentedAt { get; [UsedImplicitly] init; } = DateTimeOffset.Now;
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Text;
+        yield return CommentedAt;
+    }
 }
