e0b17a3a394cd1fee1ac7268184c9c6bf5e2f0f0
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-07-10 16:40:32 -0400

    Remove unused Entry Action pages

--- src/WebApp/Pages/Staff/EntryAction/Index.cshtml.cs
+++
@@ -1,9 +0,0 @@
-﻿using AirWeb.AppServices.Permissions;
-
-namespace AirWeb.WebApp.Pages.Staff.EntryAction;
-
-[Authorize(Policy = nameof(Policies.ActiveUser))]
-public class EntryActionIndexModel : PageModel
-{
-    public RedirectToPageResult OnGet() => RedirectToPage("../Index");
-}
