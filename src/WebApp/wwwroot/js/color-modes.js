7b64226f446df130ad68948d4faf0d90c567d450
Doug Waldron <737326+dougwaldron@users.noreply.github.com>
2024-07-12 14:10:37 -0400

    Implement consistent SVG icons (#100)

--- src/WebApp/wwwroot/js/color-modes.js
+++ src/WebApp/wwwroot/js/color-modes.js
@@ -9,15 +9,11 @@
 
     const getStoredTheme = () => localStorage.getItem('theme')
     const setStoredTheme = theme => localStorage.setItem('theme', theme)
-
-    const getPreferredTheme = () => {
-        const storedTheme = getStoredTheme()
-        return storedTheme || 'auto';
-    }
+    const getPreferredTheme = () => getStoredTheme() || 'auto'
 
     const setTheme = theme => {
-        if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            document.documentElement.setAttribute('data-bs-theme', 'dark')
+        if (theme === 'auto') {
+            document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
         } else {
             document.documentElement.setAttribute('data-bs-theme', theme)
         }
