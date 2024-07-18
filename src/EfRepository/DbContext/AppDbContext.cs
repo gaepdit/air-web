ae8f4c85ec555bf4bc284802c0b94a93b9e18cff
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-07-18 16:35:49 -0400

    Update EF repository auto-include configuration

--- src/EfRepository/DbContext/AppDbContext.cs
+++ src/EfRepository/DbContext/AppDbContext.cs
@@ -21,7 +21,7 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbCo
     // Domain entities
     public DbSet<Fce> Fces => Set<Fce>();
 
-    // Work entry/compliance event hierarchy
+    // Work entries/compliance events
     //   By default, Entity Framework uses the TPH strategy for modeling inheritance. All work entries and compliance
     //   events will be stored in a single table with a discriminator column. Each subtype and each base type are all
     //   available as DbSets for querying.
@@ -47,46 +47,58 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbCo
     {
         base.OnModelCreating(builder);
 
-        // === Auto-includes ===
+        // == Auto-includes
         // Some properties should always be included.
         // See https://learn.microsoft.com/en-us/ef/core/querying/related-data/eager#model-configuration-for-auto-including-navigations
 
-        // Users
-        builder.Entity<ApplicationUser>().Navigation(e => e.Office).AutoInclude();
+        // Entities should include any maintenance properties.
+        builder.Entity<ApplicationUser>().Navigation(user => user.Office).AutoInclude();
+        builder.Entity<Notification>().Navigation(notification => notification.NotificationType).AutoInclude();
 
-        // Work Entries
+        // FCEs should include all User data.
+        var fceEntity = builder.Entity<Fce>();
+        fceEntity.Navigation(fce => fce.DeletedBy).AutoInclude();
+        fceEntity.Navigation(fce => fce.ReviewedBy).AutoInclude();
+
+        // Work Entries should include all User data.
         var workEntryEntity = builder.Entity<WorkEntry>();
+        workEntryEntity.Navigation(entry => entry.ClosedBy).AutoInclude();
         workEntryEntity.Navigation(entry => entry.DeletedBy).AutoInclude();
+        workEntryEntity.Navigation(entry => entry.ResponsibleStaff).AutoInclude();
 
-        // === Additional configuration ===
+        // == TODO: TPH column sharing
 
 #pragma warning disable S125
-        // // FUTURE: Convert Facility ID to a string for use as primary key.
+        // // FUTURE: == Convert Facility ID to a string for use as primary key.
         // // (When Facility is added as an entity in this DbContext, the following code will be useful.)
         // builder.Entity<Facility>().Property(facility => facility.Id)
         //     .HasConversion(facilityId => facilityId.ToString(), s => (FacilityId)s);
 #pragma warning restore S125
 
-        // Let's save enums in the database as strings.
+        // == Let's save enums in the database as strings.
         // See https://learn.microsoft.com/en-us/ef/core/modeling/value-conversions?tabs=data-annotations#pre-defined-conversions
         builder.Entity<WorkEntry>().Property(entity => entity.WorkEntryType).HasConversion<string>();
         builder.Entity<ComplianceEvent>().Property(entity => entity.ComplianceEventType).HasConversion<string>();
 
         // == Collections of owned types
+        // TODO: Try using a hierarchy of inherited Comment types and see if TPH can be used to put all Comments in a single table.
+        // Collections of the same owned types by different owners should be stored in different tables.
+        // Comments should include all User data.
         // Sqlite and EF Core are in conflict on how to handle collections of owned types.
         // See: https://stackoverflow.com/a/69826156/212978
         // and: https://learn.microsoft.com/en-us/ef/core/modeling/owned-entities#collections-of-owned-types
-        builder.Entity<Fce>().OwnsMany(fce => fce.Comments, owned =>
+        fceEntity.OwnsMany(fce => fce.Comments, owned =>
         {
             owned.ToTable("Fce_Comments");
+            owned.Navigation(comment => comment.CommentBy).AutoInclude();
             if (Database.ProviderName != SqliteProvider) return;
             owned.HasKey(propertyNames: "Id");
             owned.Property(comment => comment.CommentedAt).HasConversion(new DateTimeOffsetToBinaryConverter());
         });
-
-        builder.Entity<WorkEntry>().OwnsMany(entry => entry.Comments, owned =>
+        workEntryEntity.OwnsMany(entry => entry.Comments, owned =>
         {
             owned.ToTable("WorkEntry_Comments");
+            owned.Navigation(comment => comment.CommentBy).AutoInclude();
             if (Database.ProviderName != SqliteProvider) return;
             owned.HasKey(propertyNames: "Id");
             owned.Property(comment => comment.CommentedAt).HasConversion(new DateTimeOffsetToBinaryConverter());
