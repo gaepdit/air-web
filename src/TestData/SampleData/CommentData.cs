895e9dbbbe1d0cad6fac3a40a8571621c8b38e0f
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-07-19 13:19:31 -0400

    Fix min/max issue in comment sample data

--- src/TestData/SampleData/CommentData.cs
+++ src/TestData/SampleData/CommentData.cs
@@ -8,7 +8,7 @@ public static class CommentData
     public static List<Comment> GetRandomCommentsList(int minValue = 0, int maxValue = 4)
     {
         var commentList = new List<Comment>();
-        var commentCount = new Random().Next(maxValue + minValue) - minValue;
+        var commentCount = new Random().Next(maxValue - minValue) + minValue;
         for (var i = 0; i < commentCount; i++)
         {
             commentList.Add(new Comment
