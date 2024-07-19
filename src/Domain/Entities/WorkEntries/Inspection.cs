3204bb2218c9ab500f0ec38d29585dc530696d0b
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-07-19 13:20:17 -0400

    Add abstract inspection data type for Inspection entities

--- src/Domain/Entities/WorkEntries/Inspection.cs
+++ src/Domain/Entities/WorkEntries/Inspection.cs
@@ -1,61 +1,11 @@
-﻿using System.ComponentModel;
-using System.Text.Json.Serialization;
+﻿namespace AirWeb.Domain.Entities.WorkEntries;
 
-namespace AirWeb.Domain.Entities.WorkEntries;
-
-public class Inspection : ComplianceEvent
+public class Inspection : BaseInspection
 {
     // Constructors
 
     [UsedImplicitly] // Used by ORM.
     private Inspection() { }
 
-    internal Inspection(int? id) : base(id)
-    {
-        WorkEntryType = WorkEntryType.ComplianceEvent;
-        ComplianceEventType = ComplianceEventType.Inspection;
-        IsClosed = true;
-    }
-
-    // Properties
-
-    [StringLength(18)]
-    public InspectionReason? InspectionReason { get; set; }
-
-    public DateTime InspectionStarted { get; init; }
-    public DateTime InspectionEnded { get; init; }
-
-    // TODO: Limit string length.
-    public string WeatherConditions { get; init; } = string.Empty;
-
-    // TODO: Limit string length.
-    public string InspectionGuide { get; init; } = string.Empty;
-
-    public bool FacilityOperating { get; init; }
-
-    [StringLength(15)]
-    public ComplianceStatus ComplianceStatus { get; set; }
-
-    public bool FollowupTaken { get; init; }
-}
-
-// Enums
-
-[JsonConverter(typeof(JsonStringEnumConverter))]
-public enum InspectionReason
-{
-    [Description("Planned Unannounced")] PlannedUnannounced,
-    [Description("Planned Announced")] PlannedAnnounced,
-    [Description("Unplanned")] Unplanned,
-    [Description("Complaint Investigation")] Complaint,
-    [Description("Joint EPD/EPA")] JointEpdEpa,
-    [Description("Multimedia")] Multimedia,
-    [Description("Follow-Up")] FollowUp,
-}
-
-[JsonConverter(typeof(JsonStringEnumConverter))]
-public enum ComplianceStatus
-{
-    [Description("In Compliance")] InCompliance,
-    [Description("Deviation(s) Noted")] DeviationsNoted,
+    internal Inspection(int? id) : base(id) => ComplianceEventType = ComplianceEventType.Inspection;
 }
--- src/Domain/Entities/WorkEntries/RmpInspection.cs
+++ src/Domain/Entities/WorkEntries/RmpInspection.cs
@@ -1,37 +1,11 @@
 ﻿namespace AirWeb.Domain.Entities.WorkEntries;
 
-public class RmpInspection : ComplianceEvent
+public class RmpInspection : BaseInspection
 {
     // Constructors
 
     [UsedImplicitly] // Used by ORM.
     private RmpInspection() { }
 
-    internal RmpInspection(int? id) : base(id)
-    {
-        WorkEntryType = WorkEntryType.ComplianceEvent;
-        ComplianceEventType = ComplianceEventType.RmpInspection;
-        IsClosed = true;
-    }
-
-    // Properties
-
-    [StringLength(18)]
-    public InspectionReason? InspectionReason { get; set; }
-
-    public DateTime InspectionStarted { get; init; }
-    public DateTime InspectionEnded { get; init; }
-
-    // TODO: Limit string length.
-    public string WeatherConditions { get; init; } = string.Empty;
-
-    // TODO: Limit string length.
-    public string InspectionGuide { get; init; } = string.Empty;
-
-    public bool FacilityOperating { get; init; }
-
-    [StringLength(15)]
-    public ComplianceStatus ComplianceStatus { get; set; }
-
-    public bool FollowupTaken { get; init; }
+    internal RmpInspection(int? id) : base(id) => ComplianceEventType = ComplianceEventType.RmpInspection;
 }
