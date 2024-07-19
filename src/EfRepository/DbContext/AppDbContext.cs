d91d953182ca7b97fce4e13b2715402dccc9e4f3
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-07-19 11:37:59 -0400

    Reorganize AppDbContext code

--- src/EfRepository/DbContext/AppDbContext.cs
+++ src/EfRepository/DbContext/AppDbContext.cs
@@ -6,14 +6,13 @@ using AirWeb.Domain.Identity;
 using AirWeb.EfRepository.DbContext.Configuration;
 using GaEpd.EmailService.Repository;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AirWeb.EfRepository.DbContext;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
     internal const string SqlServerProvider = "Microsoft.EntityFrameworkCore.SqlServer";
-    private const string SqliteProvider = "Microsoft.EntityFrameworkCore.Sqlite";
+    internal const string SqliteProvider = "Microsoft.EntityFrameworkCore.Sqlite";
 
     // Maintenance items
     public DbSet<NotificationType> NotificationTypes => Set<NotificationType>();
@@ -48,28 +47,13 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbCo
     {
         base.OnModelCreating(builder);
 
-        // == Auto-includes
-        // Some properties should always be included.
-        // See https://learn.microsoft.com/en-us/ef/core/querying/related-data/eager#model-configuration-for-auto-including-navigations
-
-        // Entities should include any maintenance properties.
-        builder.Entity<ApplicationUser>().Navigation(user => user.Office).AutoInclude();
-        builder.Entity<Notification>().Navigation(notification => notification.NotificationType).AutoInclude();
-
-        // FCEs should include all User data.
-        var fceEntity = builder.Entity<Fce>();
-        fceEntity.Navigation(fce => fce.DeletedBy).AutoInclude();
-        fceEntity.Navigation(fce => fce.ReviewedBy).AutoInclude();
-
-        // Work Entries should include all User data.
-        var workEntryEntity = builder.Entity<WorkEntry>();
-        workEntryEntity.Navigation(entry => entry.ClosedBy).AutoInclude();
-        workEntryEntity.Navigation(entry => entry.DeletedBy).AutoInclude();
-        workEntryEntity.Navigation(entry => entry.ResponsibleStaff).AutoInclude();
-
-        // == TPH column sharing
-        // https://learn.microsoft.com/en-us/ef/core/modeling/inheritance#shared-columns
-        AppDbContextConfiguration.ConfigureTphColumnSharing(builder);
+        // Configure Model Builder
+        builder
+            .ConfigureNavigationAutoIncludes()
+            .ConfigureTphColumnSharing()
+            .ConfigureEnumValues()
+            .ConfigureOwnedTypeCollections(Database.ProviderName)
+            .ConfigureDateTimeOffsetHandling(Database.ProviderName);
 
 #pragma warning disable S125
         // // FUTURE: == Convert Facility ID to a string for use as primary key.
@@ -77,51 +61,6 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbCo
         // builder.Entity<Facility>().Property(facility => facility.Id)
         //     .HasConversion(facilityId => facilityId.ToString(), s => (FacilityId)s);
 #pragma warning restore S125
-
-        // == Let's save enums in the database as strings.
-        // See https://learn.microsoft.com/en-us/ef/core/modeling/value-conversions?tabs=data-annotations#pre-defined-conversions
-        builder.Entity<WorkEntry>().Property(entity => entity.WorkEntryType).HasConversion<string>();
-        builder.Entity<ComplianceEvent>().Property(entity => entity.ComplianceEventType).HasConversion<string>();
-
-        // == Collections of owned types
-        // TODO: Try using a hierarchy of inherited Comment types and see if TPH can be used to put all Comments in a single table.
-        // Collections of the same owned types by different owners should be stored in different tables.
-        // Comments should include all User data.
-        // Sqlite and EF Core are in conflict on how to handle collections of owned types.
-        // See: https://stackoverflow.com/a/69826156/212978
-        // and: https://learn.microsoft.com/en-us/ef/core/modeling/owned-entities#collections-of-owned-types
-        fceEntity.OwnsMany(fce => fce.Comments, owned =>
-        {
-            owned.ToTable("Fce_Comments");
-            owned.Navigation(comment => comment.CommentBy).AutoInclude();
-            if (Database.ProviderName != SqliteProvider) return;
-            owned.HasKey(propertyNames: "Id");
-            owned.Property(comment => comment.CommentedAt).HasConversion(new DateTimeOffsetToBinaryConverter());
-        });
-        workEntryEntity.OwnsMany(entry => entry.Comments, owned =>
-        {
-            owned.ToTable("WorkEntry_Comments");
-            owned.Navigation(comment => comment.CommentBy).AutoInclude();
-            if (Database.ProviderName != SqliteProvider) return;
-            owned.HasKey(propertyNames: "Id");
-            owned.Property(comment => comment.CommentedAt).HasConversion(new DateTimeOffsetToBinaryConverter());
-        });
-
-        // == "Handling DateTimeOffset in SQLite with Entity Framework Core"
-        // https://blog.dangl.me/archive/handling-datetimeoffset-in-sqlite-with-entity-framework-core/
-        if (Database.ProviderName != SqliteProvider) return;
-        foreach (var entityType in builder.Model.GetEntityTypes())
-        {
-            // This doesn't work with owned types which instead are configured above.
-            if (entityType.FindOwnership() != null) continue;
-
-            var dateTimeOffsetProperties = entityType.ClrType.GetProperties()
-                .Where(info => info.PropertyType == typeof(DateTimeOffset) ||
-                               info.PropertyType == typeof(DateTimeOffset?));
-            foreach (var property in dateTimeOffsetProperties)
-                builder.Entity(entityType.Name).Property(property.Name)
-                    .HasConversion(new DateTimeOffsetToBinaryConverter());
-        }
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
--- src/EfRepository/DbContext/Configuration/AppDbContextConfiguration.cs
+++ src/EfRepository/DbContext/Configuration/AppDbContextConfiguration.cs
@@ -1,11 +1,49 @@
-﻿using AirWeb.Domain.Entities.WorkEntries;
+﻿using AirWeb.Domain.Entities.Fces;
+using AirWeb.Domain.Entities.WorkEntries;
+using AirWeb.Domain.Identity;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AirWeb.EfRepository.DbContext.Configuration;
 
 internal static class AppDbContextConfiguration
 {
-    internal static void ConfigureTphColumnSharing(ModelBuilder builder)
+    internal static ModelBuilder ConfigureNavigationAutoIncludes(this ModelBuilder builder)
     {
+        // Some properties should always be included.
+        // See https://learn.microsoft.com/en-us/ef/core/querying/related-data/eager#model-configuration-for-auto-including-navigations
+
+        // Entities should include any maintenance properties.
+        builder.Entity<ApplicationUser>().Navigation(user => user.Office).AutoInclude();
+        builder.Entity<Notification>().Navigation(notification => notification.NotificationType).AutoInclude();
+
+        // FCEs should include all User data.
+        var fceEntity = builder.Entity<Fce>();
+        fceEntity.Navigation(fce => fce.DeletedBy).AutoInclude();
+        fceEntity.Navigation(fce => fce.ReviewedBy).AutoInclude();
+
+        // Work Entries should include all User data.
+        var workEntryEntity = builder.Entity<WorkEntry>();
+        workEntryEntity.Navigation(entry => entry.ClosedBy).AutoInclude();
+        workEntryEntity.Navigation(entry => entry.DeletedBy).AutoInclude();
+        workEntryEntity.Navigation(entry => entry.ResponsibleStaff).AutoInclude();
+
+        return builder;
+    }
+
+    internal static ModelBuilder ConfigureEnumValues(this ModelBuilder builder)
+    {
+        // == Let's save enums in the database as strings.
+        // See https://learn.microsoft.com/en-us/ef/core/modeling/value-conversions?tabs=data-annotations#pre-defined-conversions
+        builder.Entity<WorkEntry>().Property(entity => entity.WorkEntryType).HasConversion<string>();
+        builder.Entity<ComplianceEvent>().Property(entity => entity.ComplianceEventType).HasConversion<string>();
+
+        return builder;
+    }
+
+    internal static ModelBuilder ConfigureTphColumnSharing(this ModelBuilder builder)
+    {
+        // TPH column sharing https://learn.microsoft.com/en-us/ef/core/modeling/inheritance#shared-columns
+
         var accEntity = builder.Entity<AnnualComplianceCertification>();
         var insEntity = builder.Entity<Inspection>();
         var notEntity = builder.Entity<Notification>();
@@ -61,5 +99,59 @@ internal static class AppDbContextConfiguration
         accEntity.Property(e => e.ReportsDeviations)
             .HasColumnName(nameof(AnnualComplianceCertification.ReportsDeviations));
         repEntity.Property(e => e.ReportsDeviations).HasColumnName(nameof(Report.ReportsDeviations));
+
+        return builder;
+    }
+
+    internal static ModelBuilder ConfigureOwnedTypeCollections(this ModelBuilder builder, string? dbProviderName)
+    {
+        // == Collections of owned types
+        // TODO: Try using a hierarchy of inherited Comment types and see if TPH can be used to put all Comments in a single table.
+        // Collections of the same owned types by different owners should be stored in different tables.
+        // Comments should include all User data.
+        // Sqlite and EF Core are in conflict on how to handle collections of owned types.
+        // See: https://stackoverflow.com/a/69826156/212978
+        // and: https://learn.microsoft.com/en-us/ef/core/modeling/owned-entities#collections-of-owned-types
+        builder.Entity<Fce>().OwnsMany(fce => fce.Comments, owned =>
+        {
+            owned.ToTable("Fce_Comments");
+            owned.Navigation(comment => comment.CommentBy).AutoInclude();
+            if (dbProviderName != AppDbContext.SqliteProvider) return;
+            owned.HasKey(propertyNames: "Id");
+            owned.Property(comment => comment.CommentedAt).HasConversion(new DateTimeOffsetToBinaryConverter());
+        });
+
+        builder.Entity<WorkEntry>().OwnsMany(entry => entry.Comments, owned =>
+        {
+            owned.ToTable("WorkEntry_Comments");
+            owned.Navigation(comment => comment.CommentBy).AutoInclude();
+            if (dbProviderName != AppDbContext.SqliteProvider) return;
+            owned.HasKey(propertyNames: "Id");
+            owned.Property(comment => comment.CommentedAt).HasConversion(new DateTimeOffsetToBinaryConverter());
+        });
+
+        return builder;
+    }
+
+    internal static ModelBuilder ConfigureDateTimeOffsetHandling(this ModelBuilder builder, string? dbProviderName)
+    {
+        // == "Handling DateTimeOffset in SQLite with Entity Framework Core"
+        // https://blog.dangl.me/archive/handling-datetimeoffset-in-sqlite-with-entity-framework-core/
+        if (dbProviderName != AppDbContext.SqliteProvider) return builder;
+
+        foreach (var entityType in builder.Model.GetEntityTypes())
+        {
+            // This doesn't work with owned types which instead are configured above.
+            if (entityType.FindOwnership() != null) continue;
+
+            var dateTimeOffsetProperties = entityType.ClrType.GetProperties()
+                .Where(info => info.PropertyType == typeof(DateTimeOffset) ||
+                               info.PropertyType == typeof(DateTimeOffset?));
+            foreach (var property in dateTimeOffsetProperties)
+                builder.Entity(entityType.Name).Property(property.Name)
+                    .HasConversion(new DateTimeOffsetToBinaryConverter());
+        }
+
+        return builder;
     }
 }
