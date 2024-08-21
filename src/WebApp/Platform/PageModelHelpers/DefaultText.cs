7e064ff3ad305bfa9824b183b5149531cdd26232
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-08-21 09:58:14 -0400

    Rename default display text options

---
+++ src/WebApp/Platform/PageModelHelpers/DefaultText.cs
@@ -0,0 +1,8 @@
+﻿namespace AirWeb.WebApp.Platform.PageModelHelpers;
+
+internal static class DefaultText
+{
+    public const string SelectOptionAny = "(any)";
+    public const string SelectOptionRequired = "[Select …]";
+    public const string PlaceholderNotEntered = "[Not Entered]";
+}
--- src/WebApp/Platform/PageModelHelpers/SelectOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AirWeb.WebApp.Platform.PageModelHelpers;
-
-internal static class SelectOptions
-{
-    public const string SelectTextAny = "(any)";
-    public const string SelectTextRequired = "[Select …]";
-    public const string PlaceholderNotEntered = "[Not Entered]";
-}
