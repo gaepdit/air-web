18e589d94a793ca2e822c235841dfa3a6e858d1d
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-09-06 11:06:57 -0400

    Implement new permit revocation page

---
+++ src/WebApp/Pages/Compliance/Work/PermitRevocation/Add.cshtml.cs
@@ -0,0 +1,43 @@
+using AirWeb.AppServices.Compliance.WorkEntries;
+using AirWeb.AppServices.Compliance.WorkEntries.PermitRevocations;
+using AirWeb.AppServices.ExternalEntities.Facilities;
+using AirWeb.AppServices.Permissions;
+using AirWeb.AppServices.Staff;
+using AirWeb.Domain.ComplianceEntities.WorkEntries;
+using AirWeb.WebApp.Pages.Compliance.Work.WorkEntryBase;
+using FluentValidation;
+
+namespace AirWeb.WebApp.Pages.Compliance.Work.PermitRevocation;
+
+[Authorize(Policy = nameof(Policies.ComplianceStaff))]
+public class AddModel(
+    IWorkEntryService entryService,
+    IFacilityService facilityService,
+    IStaffService staffService,
+    IValidator<PermitRevocationCreateDto> validator)
+    : AddBase(facilityService, staffService)
+{
+    private readonly IStaffService _staffService = staffService;
+
+    [BindProperty]
+    public PermitRevocationCreateDto Item { get; set; } = default!;
+
+    public async Task<IActionResult> OnGetAsync(string? facilityId)
+    {
+        EntryType = WorkEntryType.PermitRevocation;
+
+        Item = new PermitRevocationCreateDto
+        {
+            FacilityId = facilityId,
+            ResponsibleStaffId = (await _staffService.GetCurrentUserAsync()).Id,
+        };
+
+        return await DoGetAsync(facilityId);
+    }
+
+    public async Task<IActionResult> OnPostAsync(CancellationToken token)
+    {
+        EntryType = WorkEntryType.PermitRevocation;
+        return await DoPostAsync(Item, entryService, validator, token);
+    }
+}
