1c35bac7a6a3ac42951b8f36f2b0e8972157efad
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-11-22 14:23:06 -0500

    Add quick find tool to Facility page

--- src/WebApp/Pages/Facility/Index.cshtml.cs
+++ src/WebApp/Pages/Facility/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using AirWeb.AppServices.Permissions;
 using IaipDataService.Facilities;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace AirWeb.WebApp.Pages.Facility;
 
@@ -12,6 +13,13 @@ public class IndexModel(IFacilityService service) : PageModel
     [TempData]
     public bool RefreshIaipData { get; set; }
 
+    [BindProperty]
+    [Required(ErrorMessage = "Enter a facility ID.")]
+    [RegularExpression(FacilityId.FacilityIdPattern, ErrorMessage = FacilityIdFormatError)]
+    public string? FindId { get; set; }
+
+    private const string FacilityIdFormatError = "The Facility ID entered is not valid.";
+
     public async Task<IActionResult> OnGetAsync([FromQuery] bool refresh = false)
     {
         if (refresh)
@@ -19,6 +27,27 @@ public class IndexModel(IFacilityService service) : PageModel
             RefreshIaipData = true;
             return RedirectToPage();
         }
+
+        Facilities = await service.GetListAsync(RefreshIaipData);
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            Facilities = await service.GetListAsync(RefreshIaipData);
+            return Page();
+        }
+
+        if (FindId == null || !FacilityId.IsValidFormat(FindId))
+            ModelState.AddModelError(nameof(FindId), FacilityIdFormatError);
+        else if (!await service.ExistsAsync((FacilityId)FindId))
+            ModelState.AddModelError(nameof(FindId),
+                "A Facility with that ID does not exist or has not been approved in the IAIP.");
+
+        if (ModelState.IsValid)
+            return RedirectToPage("Details", routeValues: new { facilityId = FindId });
 
         Facilities = await service.GetListAsync(RefreshIaipData);
         return Page();
