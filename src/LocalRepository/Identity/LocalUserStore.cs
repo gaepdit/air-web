604cb74111d20083869e362754900230859d388f
Doug Waldron <737326+dougwaldron@users.noreply.github.com>
2025-01-14 16:26:03 -0500

    Update development settings (#231)
    * Add a dev setting to enable the web optimizer for JS and CSS files
    
    * Simplify configuring the test user in dev settings
    
    * Update documentation for IAIP data service settings

--- src/LocalRepository/Identity/LocalUserStore.cs
+++ src/LocalRepository/Identity/LocalUserStore.cs
@@ -11,52 +11,10 @@ public sealed class LocalUserStore :
     IQueryableUserStore<ApplicationUser>
 {
     public IQueryable<ApplicationUser> Users => UserStore.AsQueryable();
-    internal ICollection<ApplicationUser> UserStore { get; }
-    internal ICollection<IdentityRole> Roles { get; }
-    private List<IdentityUserRole<string>> UserRoles { get; }
-    private List<UserLogin> UserLogins { get; }
-
-    public LocalUserStore()
-    {
-        // Seed Users
-        UserStore = UserData.GetUsers.ToList();
-
-        // Seed Roles
-        Roles = UserData.GetRoles.ToList();
-
-        // Seed User Roles
-        UserRoles = new List<IdentityUserRole<string>>();
-
-        // -- admin
-        UserRoles.AddRange(Roles
-            .Select(role => new IdentityUserRole<string>
-                { RoleId = role.Id, UserId = UserStore.Single(e => e.GivenName == "Admin").Id })
-            .ToList());
-
-        // -- staff
-        var staffUserId = UserStore.Single(e => e.GivenName == "General").Id;
-        UserRoles.AddRange(new IdentityUserRole<string>[]
-        {
-            new()
-            {
-                RoleId = Roles.Single(e => e.Name == RoleName.ComplianceStaff).Id,
-                UserId = staffUserId,
-            },
-            new()
-            {
-                RoleId = Roles.Single(e => e.Name == RoleName.ComplianceSiteMaintenance).Id,
-                UserId = staffUserId,
-            },
-            new()
-            {
-                RoleId = Roles.Single(e => e.Name == RoleName.AppUserAdmin).Id,
-                UserId = staffUserId,
-            },
-        });
-
-        // Initialize Logins
-        UserLogins = new List<UserLogin>();
-    }
+    internal ICollection<ApplicationUser> UserStore { get; } = UserData.GetUsers.ToList();
+    internal ICollection<IdentityRole> Roles { get; } = UserData.GetRoles.ToList();
+    private List<IdentityUserRole<string>> UserRoles { get; } = [];
+    private List<UserLogin> UserLogins { get; } = [];
 
     // IUserStore
     public Task<string> GetUserIdAsync(ApplicationUser user, CancellationToken cancellationToken) =>
