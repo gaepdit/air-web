619f9b69c90be3d30e2be1c3a5ff83c9e7e245d4
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-01-30 08:11:59 -0500

    Disable SonarQube warning

--- src/AppServices/Compliance/Search/IComplianceSearchService.cs
+++ src/AppServices/Compliance/Search/IComplianceSearchService.cs
@@ -3,8 +3,10 @@ using GaEpd.AppLibrary.Pagination;
 namespace AirWeb.AppServices.Compliance.Search;
 
 // FUTURE: Is this reusable for Enforcement search?
+#pragma warning disable S2436 // Types and methods should not have too many generic parameters
 public interface IComplianceSearchService<in TSearchDto, TSearchResultDto, TExportDto> : IDisposable, IAsyncDisposable
     where TSearchResultDto : class
+#pragma warning restore S2436
 {
     Task<IPaginatedResult<TSearchResultDto>> SearchAsync(TSearchDto spec, PaginatedRequest paging,
         bool loadFacilities = true, CancellationToken token = default);
