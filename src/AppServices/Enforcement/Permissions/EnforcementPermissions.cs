2f3a4f7347c48ebc7a6f9ccdbbde8e10cee064e4
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-03-25 14:26:02 -0400

    Combine all enforcement action commands into actions dropdown menu

--- src/AppServices/Enforcement/Permissions/EnforcementPermissions.cs
+++ src/AppServices/Enforcement/Permissions/EnforcementPermissions.cs
@@ -1,6 +1,7 @@
 ﻿using AirWeb.AppServices.CommonInterfaces;
 using AirWeb.AppServices.Enforcement.EnforcementActionQuery;
 using AirWeb.AppServices.Permissions.Helpers;
+using AirWeb.Domain.EnforcementEntities.EnforcementActions;
 using Microsoft.Identity.Web;
 using System.Security.Claims;
 
@@ -11,14 +12,11 @@ public static class EnforcementPermissions
     public static bool CanAddComment(this ClaimsPrincipal user, IIsDeleted item) =>
         !item.IsDeleted && user.IsComplianceStaff();
 
-    public static bool CanFinalizeAction(this ClaimsPrincipal user, IActionViewDto item) =>
-        item is { IsIssued: false, IsCanceled: false } && CanFinalize(user, item);
+    public static bool CanAddResponse(this IActionViewDto item) =>
+        !item.IsDeleted && item is ResponseRequestedViewDto { IsResponseReceived: false };
 
     public static bool CanClose(this ClaimsPrincipal user, IIsClosedAndIsDeleted item) =>
         !item.IsClosed && CanFinalize(user, item);
-
-    private static bool CanFinalize(this ClaimsPrincipal user, IIsDeleted item) =>
-        !item.IsDeleted && user.IsComplianceStaff();
 
     public static bool CanDelete(this ClaimsPrincipal user, IDeletable item) =>
         !item.IsDeleted && CanManageDeletions(user);
@@ -29,6 +27,15 @@ public static class EnforcementPermissions
     public static bool CanEdit(this ClaimsPrincipal user, IIsClosedAndIsDeleted item) =>
         item is { IsClosed: false, IsDeleted: false } && user.IsComplianceStaff();
 
+    public static bool CanEdit(this ClaimsPrincipal user, IActionViewDto item) =>
+        item is { IsCanceled: false, IsDeleted: false } && user.IsComplianceStaff();
+
+    private static bool CanFinalize(this ClaimsPrincipal user, IIsDeleted item) =>
+        !item.IsDeleted && user.IsComplianceStaff();
+
+    public static bool CanFinalizeAction(this ClaimsPrincipal user, IActionViewDto item) =>
+        item is { IsDeleted: false, IsIssued: false, IsCanceled: false } && CanFinalize(user, item);
+
     public static bool CanManageDeletions(this ClaimsPrincipal user) =>
         user.IsComplianceManager();
 
@@ -37,6 +44,10 @@ public static class EnforcementPermissions
 
     public static bool CanRestore(this ClaimsPrincipal user, IDeletable item) =>
         item.IsDeleted && CanManageDeletions(user);
+
+    public static bool CanSubmitForReview(this ClaimsPrincipal user, IActionViewDto item) =>
+        item is { IsDeleted: false, IsCanceled: false, Status: EnforcementActionStatus.Draft } &&
+        user.IsComplianceStaff();
 
     public static bool CanView(this ClaimsPrincipal user, IIsClosedAndIsDeleted item) =>
         CanManageDeletions(user) ||
