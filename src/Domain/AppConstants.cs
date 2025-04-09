0649eded76bfa9435dfcc69d02e4e0d020432c51
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-04-09 13:28:08 -0400

    Add minimum and maximum dates to HTML inputs
    And fix date handling in enforcement details JavaScript

--- src/Domain/AppConstants.cs
+++ src/Domain/AppConstants.cs
@@ -10,6 +10,7 @@ public static class AppConstants
 public static class ComplianceConstants
 {
     // Compliance program dates
-    public static DateOnly EarliestComplianceDate => new DateOnly(year: EarliestWorkEntryYear, month: 1, day: 1);
     public const int EarliestWorkEntryYear = 2000;
+    public static DateOnly EarliestComplianceDate => new DateOnly(year: EarliestWorkEntryYear, month: 1, day: 1);
+    public static string EarliestComplianceDateHtmlString => EarliestComplianceDate.ToString("yyyy-MM-dd");
 }
