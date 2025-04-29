b96ffb7e7be37b471ed55f7b9faa0c8441c2945e
yoyocaleb <yoyocaleb1011@gmail.com>
2025-04-29 11:10:51 -0400

    Working on Index page for enforcement

--- src/AppServices/Enforcement/Search/EnforcementSeartchDto.cs
+++ src/AppServices/Enforcement/Search/EnforcementSeartchDto.cs
@@ -3,27 +3,26 @@ using AirWeb.AppServices.Enforcement.Search;
 using AirWeb.AppServices.Utilities;
 using IaipDataService.Facilities;
 
-namespace AirWeb.AppServices.Enforcement.Search
+namespace AirWeb.AppServices.Enforcement.Search;
+
+public enum SortByEnforcement
 {
-    public enum SortByEnforcement
-    {
-        [Description("DiscoveryDate, Id")] DiscoveryDate
-    }
-    internal class EnforcementSearchDto
-    {
-        [Display(Name = "Facility AIRS Number")]
-        [StringLength(9)]
-        public string? PartialFacilityId { get; init; }
+    [Description("DiscoveryDate, Id")] DiscoveryDate
+}
+public record EnforcementSearchDto
+{
+    [Display(Name = "Facility AIRS Number")]
+    [StringLength(9)]
+    public string? PartialFacilityId { get; init; }
 
-        public SortByEnforcement Sort { get; init; } = SortByEnforcement.DiscoveryDate;
+    public SortByEnforcement Sort { get; init; } = SortByEnforcement.DiscoveryDate;
 
-        // == Text == 
-        [Display(Name = "Notes")]
-        public string? Notes { get; init; }
-        // == Dates ==
-        [Display(Name = "Discovery Date")]
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = DateTimeFormats.DateOnlyInput, ApplyFormatInEditMode = true)]
-        public DateOnly? DiscoveryDate { get; init; }
-    }
+    // == Text == 
+    [Display(Name = "Notes")]
+    public string? Notes { get; init; }
+    // == Dates ==
+    [Display(Name = "Discovery Date")]
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = DateTimeFormats.DateOnlyInput, ApplyFormatInEditMode = true)]
+    public DateOnly? DiscoveryDate { get; init; }
 }
---
+++ src/WebApp/Pages/Enforcement/Work/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using AirWeb.AppServices.Enforcement.Permissions;
+using AirWeb.AppServices.Enforcement.Search;
+using AirWeb.AppServices.NamedEntities.Offices;
+using AirWeb.AppServices.Permissions;
+using AirWeb.AppServices.Staff;
+using AirWeb.WebApp.Models;
+using AirWeb.WebApp.Platform.Constants;
+using GaEpd.AppLibrary.Extensions;
+using GaEpd.AppLibrary.ListItems;
+using GaEpd.AppLibrary.Pagination;
+
+namespace AirWeb.WebApp.Pages.Enforcement.Work
+{
+    public class EnforcementIndexModel() : PageModel
+    {
+        public EnforcementSearchDto Spec { get; set; } = null!;
+        public bool ShowResults { get; private set; }
+        public bool UserCanViewDeletedRecords { get; private set; }
+        public IPaginatedResult<EnforcementSearchResultDto> SearchResults { get; private set; } = null!;
+        public PaginatedResultsDisplay ResultsDisplay => new(Spec, SearchResults); //add for enforce?
+        public async Task OnGetAsync(CancellationToken token = default)
+        {
+            Spec = new EnforcementSearchDto();
+        }
+        public async Task OnGetSearchAsync(EnforcementSearchDto spec, [FromQuery] int p = 1,
+            CancellationToken token = default)
+        {
+            Spec = spec;
+        }
+    }
+}
+    
+
