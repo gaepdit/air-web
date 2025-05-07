b9b837459d7f5f841cec833648069ce8514382ad
yoyocaleb <yoyocaleb1011@gmail.com>
2025-05-07 12:19:33 -0400

    able to display mock data, making changes for live data

--- src/AppServices/Enforcement/Search/EnforcementSearchService.cs
+++ src/AppServices/Enforcement/Search/EnforcementSearchService.cs
@@ -1,6 +1,9 @@
-﻿using AirWeb.AppServices.Enforcement.Search;
+﻿using AirWeb.AppServices.Compliance.Search;
+using System.Linq.Expressions;
+using AirWeb.AppServices.Enforcement.Search;
 using AirWeb.AppServices.Users;
 using AutoMapper;
+using GaEpd.AppLibrary.Domain.Repositories;
 using GaEpd.AppLibrary.Extensions;
 using GaEpd.AppLibrary.Pagination;
 using IaipDataService.Facilities;
@@ -8,17 +11,37 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace AirWeb.AppServices.Enforcement.Search
 {
-    public sealed class EnforcementSearchService(
-
+    public abstract class EnforcementSearchService<TEntity>(
+        IRepository<TEntity, int> repository,
         IFacilityService facilityService,
         IMapper mapper,
         IUserService userService,
         IAuthorizationService authorization)
+        where TEntity : class, IEntity<int>, IComplianceEntity
     {
+        protected async Task<IPaginatedResult<TResultDto>> GetSearchResultsAsync<TResultDto>(PaginatedRequest paging,
+        Expression<Func<TEntity, bool>> expression, bool loadFacilities, CancellationToken token = default)
+        where TResultDto : class, IStandardSearchResult
+        {
+            var count = await repository.CountAsync(expression, token).ConfigureAwait(false);
+
+            var list = count > 0
+                ? mapper.Map<IReadOnlyCollection<TResultDto>>(
+                    await repository.GetPagedListAsync(expression, paging, token).ConfigureAwait(false))
+                : [];
+
+            if (!loadFacilities) return new PaginatedResult<TResultDto>(list, count, paging);
+
+            foreach (var result in list)
+                result.FacilityName ??= await facilityService.GetNameAsync(result.FacilityId).ConfigureAwait(false);
+
+            return new PaginatedResult<TResultDto>(list, count, paging);
+        }
         public async Task<IPaginatedResult<EnforcementSearchResultDto>> SearchAsync(EnforcementSearchDto spec,
             PaginatedRequest paging, bool loadFacilities = true, CancellationToken token = default)
         {
-            return List<EnforcementSearchResultDto> { new EnforcementSearchResultDto { FacilityId = "00100010", DiscoveryDate = new DateOnly(2025,4,22), CaseFileStatus = Domain.EnforcementEntities.CaseFiles.CaseFileStatus.Open };
+            List<EnforcementSearchResultDto> mockData = [new EnforcementSearchResultDto { FacilityId = "00100010", DiscoveryDate = new DateOnly(2025, 4, 22), CaseFileStatus = Domain.EnforcementEntities.CaseFiles.CaseFileStatus.Open }];
+            return new PaginatedResult<EnforcementSearchResultDto>(mockData , 1, new PaginatedRequest(1, 1) );
         }
         public async Task<int> CountAsync(EnforcementSearchDto spec, CancellationToken token)
         {
---
+++ src/AppServices/Enforcement/Search/Enum.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using System.Text.Json.Serialization;
+
+namespace AirWeb.AppServices.Enforcement.Search;
+
+// FUTURE: See if any of these will work with enforcement search as well.
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum EnforcementSortBy
+{
+    [Description("Id")] IdAsc,
+    [Description("Id desc")] IdDesc,
+    [Description("FacilityId, Id")] FacilityIdAsc,
+    [Description("FacilityId desc, Id")] FacilityIdDesc,
+    [Description("WorkEntryType, Id")] WorkTypeAsc,
+    [Description("WorkEntryType desc, Id")] WorkTypeDesc,
+    [Description("EventDate, Id")] EventDateAsc,
+    [Description("EventDate desc, Id")] EventDateDesc,
+    [Description("Year, Id")] YearAsc,
+    [Description("Year desc, Id")] YearDesc,
+}
+
+// "(Any)" (null) = no filtering
+// "Yes" = only if value is true
+// "No" = only if value is false
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum YesNoAny
+{
+    Yes = 1,
+    No = 0,
+}
+
+// "(Any)" (null) = no filtering
+// "Closed" = only if "closed" value is true
+// "Open" = only if "closed" value is false
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum ClosedOpenAny
+{
+    Closed = 1,
+    Open = 0,
+}
+
+// "Not Deleted" (null) = only non-deleted entries
+// "Deleted" = only deleted entries
+// "All" = both deleted and not-deleted entries
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum DeleteStatus
+{
+    Deleted = 0,
+    All = 1,
+}
