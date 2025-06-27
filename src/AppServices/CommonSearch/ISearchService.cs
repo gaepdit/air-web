cb5f1c5bf30a98bfb643bb6f8567d127affa6308
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-06-27 14:21:05 -0400

    Disable generic parameters warning

--- src/AppServices/CommonSearch/ISearchService.cs
+++ src/AppServices/CommonSearch/ISearchService.cs
@@ -2,7 +2,9 @@ using GaEpd.AppLibrary.Pagination;
 
 namespace AirWeb.AppServices.CommonSearch;
 
+#pragma warning disable S2436 // Types and methods should not have too many generic parameters
 public interface ISearchService<in TSearchDto, TResultDto, TExportDto> : IDisposable, IAsyncDisposable
+#pragma warning restore S2436
     where TSearchDto : ISearchDto<TSearchDto>
     where TResultDto : class, ISearchResult
     where TExportDto : ISearchResult
