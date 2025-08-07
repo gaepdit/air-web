5d0255bd51f97569e0f84faab1d34bc57978842a
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-08-07 15:23:32 -0400

    WIP: Case File query projection

--- src/Domain/EnforcementEntities/CaseFiles/ICaseFileRepository.cs
+++ src/Domain/EnforcementEntities/CaseFiles/ICaseFileRepository.cs
@@ -2,8 +2,6 @@
 
 public interface ICaseFileRepository : IRepositoryWithMapping<CaseFile, int>, ICommentRepository<int>
 {
-    public static string[] IncludeDetails => [nameof(CaseFile.EnforcementActions), nameof(CaseFile.ComplianceEvents)];
-
     // Will return the next available ID if the repository requires it for adding new entities (e.g., local repository).
     // Will return null if the repository creates a new ID on insert (e.g., Entity Framework).
     int? GetNextId();
