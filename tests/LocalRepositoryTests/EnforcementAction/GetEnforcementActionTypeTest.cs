9a8561937c7a21a1d13ef09842a7645f9fffe608
yoyocaleb <yoyocaleb1011@gmail.com>
2025-08-25 12:27:02 -0400

    initial test created

---
+++ tests/LocalRepositoryTests/EnforcementAction/GetEnforcementActionTypeTest.cs
@@ -0,0 +1,13 @@
+﻿using AirWeb.LocalRepository.Repositories;
+using AirWeb.TestData.Enforcement;
+
+namespace LocalRepositoryTests.EnforcementAction;
+
+[TestFixture]
+public class GetEnforcementActionTypeTest
+{
+    private LocalEnforcementActionRepository _repository;
+
+    [SetUp]
+    
+}
