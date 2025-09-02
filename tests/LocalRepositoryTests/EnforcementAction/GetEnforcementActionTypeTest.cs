a1484af4404b676714176ee2911ff9f40c78a5e7
yoyocaleb <yoyocaleb1011@gmail.com>
2025-09-02 11:38:13 -0400

    GetEnforcementActionType test created

--- tests/LocalRepositoryTests/EnforcementAction/GetEnforcementActionTypeTest.cs
+++ tests/LocalRepositoryTests/EnforcementAction/GetEnforcementActionTypeTest.cs
@@ -1,5 +1,8 @@
-﻿using AirWeb.LocalRepository.Repositories;
+﻿using AirWeb.Domain.BaseEntities.Interfaces;
+using AirWeb.Domain.EnforcementEntities.EnforcementActions;
+using AirWeb.LocalRepository.Repositories;
 using AirWeb.TestData.Enforcement;
+
 
 namespace LocalRepositoryTests.EnforcementAction;
 
@@ -9,5 +12,35 @@ public class GetEnforcementActionTypeTest
     private LocalEnforcementActionRepository _repository;
 
     [SetUp]
+    public void SetUp() => _repository = RepositoryHelper.GetEnforcementActionRepository();
     
+    [TearDown]
+    public void TearDown() => _repository.Dispose();
+    
+    [Test]
+    public async Task GetEnforcementActionType_WhenIdExist()
+    {
+        // Arrange
+        var existingEAT = EnforcementActionData.GetData.First();
+        var expected = existingEAT.ActionType;
+
+        // Act
+        var results = await _repository.GetEnforcementActionType(existingEAT.Id);
+
+        // Assert
+        results.Should().Be(expected);
+    }
+    [Test]
+    public async Task GetEnforcementActionType_WhenIdDoesNotExist()
+    {
+        // Arrange
+        var nonExistingEAT = Guid.NewGuid();
+
+        // Act
+        var results = await _repository.GetEnforcementActionType(nonExistingEAT);
+
+        // Assert
+        results.Should().BeNull();
+    }
 }
+    
