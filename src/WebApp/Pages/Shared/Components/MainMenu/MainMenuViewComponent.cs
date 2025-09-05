9f92ced81bbb314b77c1b0d30750a99898eead0d
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-09-05 11:18:47 -0400

    Hide inaccessible items from the main menu

--- src/WebApp/Pages/Shared/Components/MainMenu/MainMenuViewComponent.cs
+++ src/WebApp/Pages/Shared/Components/MainMenu/MainMenuViewComponent.cs
@@ -8,13 +8,21 @@ public class MainMenuViewComponent(IAuthorizationService authorization) : ViewCo
     public async Task<IViewComponentResult> InvokeAsync(PageModel model) =>
         View("Default", new MenuParams
         {
-            IsLoginPage = model is LoginModel,
-            IsActiveUser = await authorization.Succeeded(User, Policies.ActiveUser),
+            ShowSearchMenu = await authorization.Succeeded(User, Policies.Staff),
+            ShowMoreMenu = await authorization.Succeeded(User, Policies.ViewAdminPages),
+            ShowSiteMaintenanceMenu = await authorization.Succeeded(User, Policies.ViewSiteMaintenancePage),
+            ShowUsersMenu = await authorization.Succeeded(User, Policies.ViewUsersPage),
+            ShowAccountMenu = User.Identity is { IsAuthenticated: true },
+            ShowLoginLink = model is not LoginModel,
         });
 
     public record MenuParams
     {
-        public bool IsLoginPage { get; init; }
-        public bool IsActiveUser { get; init; }
+        public bool ShowSearchMenu { get; init; }
+        public bool ShowMoreMenu { get; init; }
+        public bool ShowSiteMaintenanceMenu { get; init; }
+        public bool ShowUsersMenu { get; init; }
+        public bool ShowAccountMenu { get; init; }
+        public bool ShowLoginLink { get; init; }
     }
 }
