49a6457c8381fa6ddf1f2bf47df3cc4dc3cca810
yoyocaleb <yoyocaleb1011@gmail.com>
2025-09-10 10:47:09 -0400

    Created EnforcementActionRepository

---
+++ src/EfRepository/Repositories/EnforcementActionRepository.cs
@@ -0,0 +1,25 @@
+﻿using AirWeb.Domain.Comments;
+using AirWeb.Domain.EnforcementEntities.EnforcementActions;
+using AirWeb.EfRepository.Contexts;
+using IaipDataService.Facilities;
+
+namespace AirWeb.EfRepository.Repositories;
+
+public sealed class EnforcementActionRepository(AppDbContext context)
+    : BaseRepository<EnforcementAction, Guid, AppDbContext>(context), IEnforcementActionRepository
+{
+    public Task<ConsentOrder> GetConsentOrder(Guid id, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<EnforcementActionType?> GetEnforcementActionType(Guid id, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> OrderIdExists(short orderId, Guid? ignoreActionId, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+}
