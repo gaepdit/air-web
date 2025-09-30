5661bfb8a03b7163387a1c249ec2a9a34463fc00
Doug Waldron <737326+dougwaldron@users.noreply.github.com>
2025-09-30 08:39:05 -0400

    Improve handling of missing office for current user (#386)

--- src/WebApp/Pages/Account/Index.cshtml.cs
+++ src/WebApp/Pages/Account/Index.cshtml.cs
@@ -2,6 +2,7 @@ using AirWeb.AppServices.AuthorizationPolicies;
 using AirWeb.AppServices.Staff;
 using AirWeb.AppServices.Staff.Dto;
 using AirWeb.Domain.Identity;
+using AirWeb.WebApp.Models;
 
 namespace AirWeb.WebApp.Pages.Account;
 
@@ -16,6 +17,11 @@ public class AccountIndexModel : PageModel
     {
         DisplayStaff = await staffService.GetCurrentUserAsync();
         Roles = await staffService.GetAppRolesAsync(DisplayStaff.Id);
+
+        if (DisplayStaff.Office is null)
+            TempData.AddDisplayMessage(DisplayMessage.AlertContext.Warning,
+                message: "Your Office must be set. Select “Edit Profile” and choose an Office from the list.");
+
         return Page();
     }
 }
