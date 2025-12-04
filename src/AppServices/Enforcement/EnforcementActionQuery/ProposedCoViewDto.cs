71911f720023b2c77cfd7c0b4d4c0cb971f5bf46
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-12-04 11:38:57 -0500

    Suppress warning on PCO view DTO

--- src/AppServices/Enforcement/EnforcementActionQuery/ProposedCoViewDto.cs
+++ src/AppServices/Enforcement/EnforcementActionQuery/ProposedCoViewDto.cs
@@ -1,3 +1,7 @@
-﻿namespace AirWeb.AppServices.Enforcement.EnforcementActionQuery;
+﻿using System.Diagnostics.CodeAnalysis;
 
+namespace AirWeb.AppServices.Enforcement.EnforcementActionQuery;
+
+[SuppressMessage("Minor Code Smell", "S2094:Classes should not be empty",
+    Justification = "This record type triggers a different partial view in the UI.")]
 public record ProposedCoViewDto : ResponseRequestedViewDto;
