abfb053e4cbd615b37d9f8696ea94c1f3daf8c98
Doug Waldron <737326+dougwaldron@users.noreply.github.com>
2025-12-10 11:27:24 -0500

    Refactor libman to use jsDelivr and update libraries (#437)

--- src/WebApp/wwwroot/js/anchorInit.js
+++ src/WebApp/wwwroot/js/anchorInit.js
@@ -1,5 +1,6 @@
 ﻿if (typeof AnchorJS === 'function') {
     const anchors = new AnchorJS();
+    // DOMContentLoaded was tested to be the best place to call anchors.add()
     document.addEventListener('DOMContentLoaded', function () {
         anchors.options.placement = "left";
         anchors.add('h2:not(.no-anchor), h3:not(.no-anchor)');
