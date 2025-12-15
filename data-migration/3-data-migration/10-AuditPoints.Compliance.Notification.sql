6ea77bed01ab11f1a8e5bef9e16c0a70a9b73e9e
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-12-15 14:33:43 -0500

    Migrate Compliance Monitoring audit points

---
+++ data-migration/3-data-migration/10-AuditPoints.Compliance.Notification.sql
@@ -0,0 +1,42 @@
+-- insert into AirWeb.dbo.AuditPoints (Id, What, WhoId, [When], Discriminator, ComplianceWorkId)
+select newid()                                  as Id,
+       'Added'                                  as What,
+       ui.Id                                    as WhoId,
+       i.DATMODIFINGDATE
+           at time zone 'Eastern Standard Time' as [When],
+       'ComplianceWorkAuditPoint'               as Discriminator,
+       i.STRTRACKINGNUMBER                      as ComplianceWorkId
+
+from AIRBRANCH.dbo.SSCPITEMMASTER i
+    inner join AirWeb.dbo.AspNetUsers ui
+        on ui.IaipUserId = i.STRMODIFINGPERSON
+
+where i.STRDELETE is null
+  and i.STREVENTTYPE = '05'
+
+order by i.DATMODIFINGDATE, i.STRTRACKINGNUMBER;
+
+-- insert into AirWeb.dbo.AuditPoints (Id, What, WhoId, [When], Discriminator, ComplianceWorkId)
+select newid()                                  as Id,
+       'Edited'                                 as What,
+       ud.Id                                    as WhoId,
+       d.DATMODIFINGDATE
+           at time zone 'Eastern Standard Time' as [When],
+       'ComplianceWorkAuditPoint'               as Discriminator,
+       i.STRTRACKINGNUMBER                      as ComplianceWorkId
+
+from AIRBRANCH.dbo.SSCPITEMMASTER i
+    inner join AIRBRANCH.dbo.SSCPNOTIFICATIONS d
+        on d.STRTRACKINGNUMBER = i.STRTRACKINGNUMBER
+    inner join AirWeb.dbo.AspNetUsers ud
+        on ud.IaipUserId = d.STRMODIFINGPERSON
+
+where i.STRDELETE is null
+  and i.STREVENTTYPE = '05'
+  and datediff(second, i.DATMODIFINGDATE, d.DATMODIFINGDATE) > 10
+
+order by d.DATMODIFINGDATE, i.STRTRACKINGNUMBER;
+
+select *
+from AirWeb.dbo.AuditPoints
+where Discriminator = 'ComplianceWorkAuditPoint';
