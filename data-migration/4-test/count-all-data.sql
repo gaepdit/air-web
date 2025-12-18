0f432a94a9d7ad99a8e5c148d39c8dcba38fb725
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-12-18 07:48:59 -0500

    Reorganize some scripts

--- data-migration/4-test/count-all-data.sql
+++ data-migration/4-test/count-all-data.sql
@@ -54,3 +54,58 @@ select 'SSCP_EnforcementEvents' as [table_name],
        count(distinct concat(EnforcementNumber, '_', TrackingNumber))
                                 as [count_distinct]
 from AIRBRANCH.dbo.SSCP_EnforcementEvents
+
+---
+
+select 'AspNetUsers' as [table_name],
+       count(*)      as [count]
+from AirWeb.dbo.AspNetUsers
+union
+select 'AuditPoints' as [table_name],
+       count(*)      as [count]
+from AirWeb.dbo.AuditPoints
+union
+select 'CaseFileComplianceEvents' as [table_name],
+       count(*)                   as [count]
+from AirWeb.dbo.CaseFileComplianceEvents
+union
+select 'CaseFiles' as [table_name],
+       count(*)    as [count]
+from AirWeb.dbo.CaseFiles
+union
+select 'Comments' as [table_name],
+       count(*)   as [count]
+from AirWeb.dbo.Comments
+union
+select 'ComplianceWork' as [table_name],
+       count(*)         as [count]
+from AirWeb.dbo.ComplianceWork
+union
+select 'EmailLogs' as [table_name],
+       count(*)    as [count]
+from AirWeb.dbo.EmailLogs
+union
+select 'EnforcementActionReviews' as [table_name],
+       count(*)                   as [count]
+from AirWeb.dbo.EnforcementActionReviews
+union
+select 'EnforcementActions' as [table_name],
+       count(*)             as [count]
+from AirWeb.dbo.EnforcementActions
+union
+select 'Fces'   as [table_name],
+       count(*) as [count]
+from AirWeb.dbo.Fces
+union
+select 'Lookups' as [table_name],
+       count(*)  as [count]
+from AirWeb.dbo.Lookups
+union
+select 'StipulatedPenalties' as [table_name],
+       count(*)              as [count]
+from AirWeb.dbo.StipulatedPenalties
+union
+select 'ViolationTypes' as [table_name],
+       count(*)         as [count]
+from AirWeb.dbo.ViolationTypes
+;
