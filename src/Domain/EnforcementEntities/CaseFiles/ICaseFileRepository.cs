9d5d432a674c856df0fc400c78ce299c34db6d05
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-12-18 10:50:47 -0500

    Fix missing audit points on Case Files

--- src/Domain/EnforcementEntities/CaseFiles/ICaseFileRepository.cs
+++ src/Domain/EnforcementEntities/CaseFiles/ICaseFileRepository.cs
@@ -11,6 +11,7 @@ public interface ICaseFileRepository : IRepositoryWithMapping<CaseFile, int>, IC
         nameof(CaseFile.ComplianceEvents),
         nameof(CaseFile.EnforcementActions),
         nameof(CaseFile.ViolationType),
+        nameof(CaseFile.AuditPoints),
         $"{nameof(CaseFile.EnforcementActions)}.{nameof(ConsentOrder.StipulatedPenalties)}",
         $"{nameof(CaseFile.EnforcementActions)}.{nameof(EnforcementAction.Reviews)}",
         $"{nameof(CaseFile.EnforcementActions)}.{nameof(EnforcementAction.Reviews)}.{nameof(EnforcementActionReview.RequestedOf)}",
