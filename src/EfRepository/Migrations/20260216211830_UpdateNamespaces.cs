7bfc0b0e246b7a70b3bb3bdfd19b8ae8810eedb7
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-02-16 16:19:57 -0500

    Update EF migration model snapshot

---
+++ src/EfRepository/Migrations/20260216211830_UpdateNamespaces.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AirWeb.EfRepository.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateNamespaces : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Method intentionally left empty.
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Method intentionally left empty.
+        }
+    }
+}
