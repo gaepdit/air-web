65dff42261bbb5b83f94606747791bc37c5f4f4b
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-02-18 10:51:33 -0500

    Simplify role category naming

--- src/Domain.Core/AppRoles/GeneralRole.cs
+++ src/Domain.Core/AppRoles/GeneralRole.cs
@@ -10,18 +10,18 @@ public static class GeneralRole
     public const string SiteMaintenance = nameof(SiteMaintenance);
     public const string AppUserAdmin = nameof(AppUserAdmin);
 
-    private const string GeneralCategory = "General";
+    private const string Category = "General";
 
     public static void AddRoles() => AppRole.AddAppRoles(GeneralStaffRole, SiteMaintenanceRole, UserAdminRole);
 
     // The following static Role objects are used for displaying role information in the UI.
-    public static AppRole GeneralStaffRole { get; } = new(GeneralStaff, GeneralCategory, AppRole.RoleType.Staff,
+    public static AppRole GeneralStaffRole { get; } = new(GeneralStaff, Category, AppRole.RoleType.Staff,
         "General Staff", "Can access search results and read-only views of most data.");
 
-    public static AppRole SiteMaintenanceRole { get; } = new(SiteMaintenance, GeneralCategory,
+    public static AppRole SiteMaintenanceRole { get; } = new(SiteMaintenance, Category,
         AppRole.RoleType.SiteMaintenance, "Site Maintenance",
         "Can update values in common lookup tables (drop-down lists).");
 
-    public static AppRole UserAdminRole { get; } = new(AppUserAdmin, GeneralCategory, AppRole.RoleType.UserAdmin,
+    public static AppRole UserAdminRole { get; } = new(AppUserAdmin, Category, AppRole.RoleType.UserAdmin,
         "User Account Admin", "Can edit all user profiles and roles.");
 }
