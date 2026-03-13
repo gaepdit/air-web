c9d2255601fe06132136884be718ded7a0edc717
yoyocaleb <yoyocaleb1011@gmail.com>
2026-03-13 10:58:47 -0400

    AIrProgram.cs created

---
+++ src/IaipDataService/Facilities/AirProgram.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IaipDataService.Facilities;
+
+public record AirPrograms(
+    string Code,
+    string Name
+);
+
