2f621f11b7c7225784f2ecf478caa9e93d4c1b4f
yoyocaleb <yoyocaleb1011@gmail.com>
2026-03-13 11:17:35 -0400

    AllAirPrograms list created

---
+++ src/Domain.Compliance/Data/AirPrograms.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirWeb.Domain.Compliance.Data;
+
+public static class CommonoData
+{
+    private static List<AirPrograms> AllAirPrograms { get; } =
+    [
+        new("CAASIP", "SIP"),
+        new("CAAFIP","Federal SIP"),
+        new("CAANFRP","Non-Federal SIP"),
+        new("CAACFC","CFC Tracking"),
+        new("CAAPSD","PSD"),
+        new("CAANSR","NSR"),
+        new("CAANESH","NESHAP"),
+        new("CAANSPS","ASPS"),
+        new("CAAAR","Acid Precipitation"),
+        new("CAAFENF","Federally-Enforceable Requirement"),
+        new("CAAFESOP","FESOP"),
+        new("CAAGHG","Greenhouse Gas Reporting Rule"),
+        new("CAANAM","Native American"),
+        new("CAAMACT","MACT"),
+        new("CAAPARGDC","Prevention of Accidental Release"),
+        new("CAATIP","Tribal Implementation Plan (TIP)"),
+        new("CAATVP","Title V"),
+        new("CAAGACTM","40 CFR Part 63 Area Sources"),
+        new("CAANSPSM","NSPS (Non-Major)"),
+        new("CAARMP","Risk Management Program"),
+    ];
+}
