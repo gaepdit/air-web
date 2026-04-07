00b4311700840db1357bcbcde5e98997f65f9859
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-04-07 12:48:09 -0400

    Enable editing LON resolved date

---
+++ src/AppServices.Compliance/Enforcement/EnforcementActionCommand/LetterOfNoncomplianceEditDto.cs
@@ -0,0 +1,28 @@
+﻿using AirWeb.AppServices.Core.Utilities;
+using AirWeb.Domain.Core.Data.DataAttributes;
+using FluentValidation;
+
+namespace AirWeb.AppServices.Compliance.Enforcement.EnforcementActionCommand;
+
+public record LetterOfNoncomplianceEditDto : EnforcementActionEditDto
+{
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = DateTimeFormats.DateOnlyInput, ApplyFormatInEditMode = true)]
+    [Display(Name = "Resolved")]
+    [MaxDate]
+    public DateOnly? ResolvedDate { get; init; }
+}
+
+public class LetterOfNoncomplianceEditValidator : AbstractValidator<LetterOfNoncomplianceEditDto>
+{
+    public LetterOfNoncomplianceEditValidator()
+    {
+        RuleFor(dto => dto)
+           .Must(dto => dto.IssueDate <= DateOnly.FromDateTime(DateTime.Today))
+           .WithMessage("The issued date cannot be in the future.")
+           .Must(dto => dto.ResolvedDate == null || dto.ResolvedDate <= DateOnly.FromDateTime(DateTime.Today))
+           .WithMessage("The resolved date cannot be in the future.")
+           .Must(dto => dto.ResolvedDate == null || dto.ResolvedDate >= dto.IssueDate)
+           .WithMessage("The resolved date cannot be earlier than the issued date.");
+    }
+}
