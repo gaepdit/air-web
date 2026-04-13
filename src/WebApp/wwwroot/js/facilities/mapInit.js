0d83f8bdd30da206d4f5ab61bd68d1300e87e966
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-04-13 10:20:05 -0400

    Fix SonarCloud warnings

--- src/WebApp/wwwroot/js/facilities/mapInit.js
+++ src/WebApp/wwwroot/js/facilities/mapInit.js
@@ -10,42 +10,6 @@ function initMap() {
     // Fix the marker icon path.
     L.Icon.Default.prototype.options.imagePath = '/lib/leaflet/dist/images/';
 
-    // Define alert functions.
-    const removeAlert = function () {
-        const alertElement = document.getElementById('map-warning');
-        if (alertElement !== null) alertElement.remove();
-    }
-
-    const showAlert = function () {
-        const alertElement = document.createElement('div');
-        alertElement.innerText = 'Warning: Could not access location.';
-        alertElement.classList.add('alert', 'alert-warning', 'alert-dismissible', 'fade', 'show');
-        alertElement.setAttribute('id', 'map-warning');
-        alertElement.setAttribute('role', 'alert');
-
-        const closeButton = document.createElement('button');
-        closeButton.classList.add('btn-close');
-        closeButton.setAttribute('type', 'button');
-        closeButton.setAttribute('data-bs-dismiss', 'alert');
-        closeButton.setAttribute('aria-label', 'Close');
-        alertElement.insertAdjacentElement('beforeend', closeButton);
-
-        const mapElement = document.getElementById('facility-map');
-        mapElement.parentNode.insertBefore(alertElement, mapElement);
-    }
-
-    // Define new marker function.
-    const newMarker = function (f) {
-        const info = '<div class="facility-map-info">' +
-            `<div class="facility-map-title">${f.Name}</div>` +
-            `<div>${f.Location}</div>` +
-            `<div class="facility-map-link"><a target="_blank" href="Details/${f.Id}">${f.Id}</a></div>` +
-            '</div>';
-        const marker = L.marker([f.GeoCoordinates.Latitude, f.GeoCoordinates.Longitude], { title: f.Name });
-        marker.bindPopup(info);
-        return marker;
-    }
-
     map.whenReady(function () {
         // Add a location control.
         L.control.locate({ initialZoomLevel: 13, onLocationError: showAlert }).addTo(map);
@@ -62,6 +26,42 @@ function initMap() {
         for (const f of facilities) if (f.GeoCoordinates) markers.addLayer(newMarker(f));
         map.addLayer(markers);
     });
+}
+
+// Alert functions.
+function removeAlert() {
+    const alertElement = document.getElementById('map-warning');
+    if (alertElement !== null) alertElement.remove();
+}
+
+function showAlert() {
+    const alertElement = document.createElement('div');
+    alertElement.innerText = 'Warning: Could not access location.';
+    alertElement.classList.add('alert', 'alert-warning', 'alert-dismissible', 'fade', 'show');
+    alertElement.setAttribute('id', 'map-warning');
+    alertElement.setAttribute('role', 'alert');
+
+    const closeButton = document.createElement('button');
+    closeButton.dataset.bsDismiss = 'alert';
+    closeButton.classList.add('btn-close');
+    closeButton.setAttribute('type', 'button');
+    closeButton.setAttribute('aria-label', 'Close');
+    alertElement.append(closeButton);
+
+    const mapElement = document.getElementById('facility-map');
+    mapElement.parentNode.insertBefore(alertElement, mapElement);
+}
+
+// New marker function.
+function newMarker(f) {
+    const info = '<div class="facility-map-info">' +
+        `<div class="facility-map-title">${f.Name}</div>` +
+        `<div>${f.Location}</div>` +
+        `<div class="facility-map-link"><a target="_blank" href="Details/${f.Id}">${f.Id}</a></div>` +
+        '</div>';
+    const marker = L.marker([f.GeoCoordinates.Latitude, f.GeoCoordinates.Longitude], { title: f.Name });
+    marker.bindPopup(info);
+    return marker;
 }
 
 initMap();
