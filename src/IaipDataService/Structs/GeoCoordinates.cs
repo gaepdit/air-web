7603f2eae851880afe55f774594aea4526e264c3
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-04-14 09:06:23 -0400

    Fix Apple Maps link

--- src/IaipDataService/Structs/GeoCoordinates.cs
+++ src/IaipDataService/Structs/GeoCoordinates.cs
@@ -3,5 +3,5 @@
 public readonly record struct GeoCoordinates(decimal Latitude, decimal Longitude)
 {
     public string GoogleMapsUrl => $"https://www.google.com/maps/search/?api=1&query={Latitude}%2C{Longitude}";
-    public string AppleMapsUrl => $"http://maps.apple.com/?q={Latitude}%2C{Longitude}";
+    public string AppleMapsUrl => $"https://maps.apple.com/?q={Latitude}%2C{Longitude}";
 }
