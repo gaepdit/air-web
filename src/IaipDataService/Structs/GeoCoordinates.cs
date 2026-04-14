d3853f7b1b0d27d25bff802e8f0f2d995f606548
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-04-14 08:26:17 -0400

    Add links to external mapping sites

--- src/IaipDataService/Structs/GeoCoordinates.cs
+++ src/IaipDataService/Structs/GeoCoordinates.cs
@@ -1,3 +1,7 @@
 ﻿namespace IaipDataService.Structs;
 
-public readonly record struct GeoCoordinates(decimal Latitude, decimal Longitude);
+public readonly record struct GeoCoordinates(decimal Latitude, decimal Longitude)
+{
+    public string GoogleMapsUrl => $"https://www.google.com/maps/search/?api=1&query={Latitude}%2C{Longitude}";
+    public string AppleMapsUrl => $"http://maps.apple.com/?q={Latitude}%2C{Longitude}";
+}
