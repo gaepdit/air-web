8d64d2371e86c8e7f7b6cd2c94103dd5d3a1602c
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-04-15 15:18:01 -0400

    Remove TOC if there are no headings to show

--- src/WebApp/wwwroot/js/table-of-contents.js
+++ src/WebApp/wwwroot/js/table-of-contents.js
@@ -2,18 +2,23 @@
     const toc = document.getElementById('toc');
     const headers = document.querySelectorAll(headerTags.join(', '));
 
-    headers.forEach((header, index) => {
-        // create ID if H tag has none
-        if (!header.id) header.id = `section${index + 1}`;
+    if (headers.length > 0) {
+        headers.forEach((header, index) => {
+            // create ID if H tag has none
+            if (!header.id) header.id = `section${index + 1}`;
 
-        const link = document.createElement('a');
-        link.href = `#${header.id}`;
-        link.textContent = header.textContent;
-        link.classList.add('list-group-item', 'list-group-item-action', 'toc-item', 'px-1', 'py-1');
+            const link = document.createElement('a');
+            link.href = `#${header.id}`;
+            link.textContent = header.textContent;
+            link.classList.add('list-group-item', 'list-group-item-action', 'toc-item', 'px-1', 'py-1');
 
-        // indent H3
-        if (header.tagName === 'H3') link.classList.add('ps-3');
+            // indent H3
+            if (header.tagName === 'H3') link.classList.add('ps-3');
 
-        toc.appendChild(link);
-    });
+            toc.appendChild(link);
+        });
+    } else {
+        const tocContainer = document.getElementById('toc-container');
+        if (tocContainer) tocContainer.remove();
+    }
 }
