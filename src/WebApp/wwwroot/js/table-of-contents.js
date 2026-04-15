ef9badb592e2001bfc93a999f941cf1c8d808555
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-04-15 12:45:14 -0400

    Adjust layout

--- src/WebApp/wwwroot/js/table-of-contents.js
+++ src/WebApp/wwwroot/js/table-of-contents.js
@@ -1,24 +1,19 @@
 ﻿function createTOC(headerTags) {
     const toc = document.getElementById('toc');
-
-    const tags = headerTags.join(', ');
-    const headers = document.querySelectorAll(tags);
+    const headers = document.querySelectorAll(headerTags.join(', '));
 
     headers.forEach((header, index) => {
-        // creates ID if H tag has none
-        if (!header.id) {
-            header.id = `section${index + 1}`;
-        }
+        // create ID if H tag has none
+        if (!header.id) header.id = `section${index + 1}`;
 
         const link = document.createElement('a');
         link.href = `#${header.id}`;
         link.textContent = header.textContent;
-        link.classList.add('list-group-item', 'list-group-item-action', 'toc-item', 'py-1', 'py-lg-2');
+        link.classList.add('list-group-item', 'list-group-item-action', 'toc-item', 'px-1', 'py-1');
 
         // indent H3
-        if (header.tagName === 'H3') {
-            link.classList.add('ps-3');
-        }
+        if (header.tagName === 'H3') link.classList.add('ps-3');
+
         toc.appendChild(link);
     });
 }
