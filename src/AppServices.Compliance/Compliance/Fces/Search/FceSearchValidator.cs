65865da488875cb15f639dae18cc609c4404af9f
yoyocaleb <yoyocaleb1011@gmail.com>
2026-04-17 10:43:10 -0400

    Created FceSearchValidator.cs

---
+++ src/AppServices.Compliance/Compliance/Fces/Search/FceSearchValidator.cs
@@ -0,0 +1,27 @@
+﻿using AirWeb.Domain.Compliance;
+using AirWeb.Domain.Compliance.ComplianceEntities.Fces;
+using FluentValidation;
+using IaipDataService.Facilities;
+
+namespace AirWeb.AppServices.Compliance.Compliance.Fces.Search;
+
+public class FceSearchValidator : AbstractValidator<FceSearchDto>
+{
+
+    public FceSearchValidator()
+    {
+        var today = DateOnly.FromDateTime(DateTime.Today);
+
+        RuleFor(dto => dto.DateFrom)
+            .Must(date => date <= today)
+            .WithMessage("The FCE From Date cannot be in the future.");
+
+        RuleFor(dto => dto.DateTo)
+            .Must(date => date <= today)
+            .WithMessage("The FCE To Date cannot be in the future.")
+            .Must((dto, date) => date >= dto.DateFrom)
+            .WithMessage("The FCE To Date must be later than the From Date.");
+    }
+}
+
+
