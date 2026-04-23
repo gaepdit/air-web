a7d89044ac208428a50b87f84ea7d4ba6d16399f
yoyocaleb <yoyocaleb1011@gmail.com>
2026-04-23 11:44:45 -0400

    checks to see if facility airs is valid

--- src/AppServices.Compliance/Compliance/Fces/Search/FceSearchValidator.cs
+++ src/AppServices.Compliance/Compliance/Fces/Search/FceSearchValidator.cs
@@ -7,9 +7,10 @@ namespace AirWeb.AppServices.Compliance.Compliance.Fces.Search;
 
 public class FceSearchValidator : AbstractValidator<FceSearchDto>
 {
-
-    public FceSearchValidator()
+    private readonly IFacilityService _service;
+    public FceSearchValidator(IFacilityService service)
     {
+        _service = service;
         var today = DateOnly.FromDateTime(DateTime.Today);
 
         RuleFor(dto => dto.DateFrom)
@@ -22,6 +23,12 @@ public class FceSearchValidator : AbstractValidator<FceSearchDto>
             .WithMessage("The FCE To Date cannot be in the future.")
             .Must((dto, date) => dto.DateFrom == default || date >= dto.DateFrom || date == null)
             .WithMessage("The FCE To Date must be later than the From Date.");
+
+        RuleFor(dto => dto.FacilityId)
+            .MustAsync(async (id, cancellation) =>
+            await service.ExistsAsync((FacilityId)id!))
+            .WithMessage("A Facility with that ID does not exist or has not been approved in the IAIP.")
+            .When(dto => !string.IsNullOrWhiteSpace(dto.FacilityId));
     }
 
 }
