2a302b61eab29b79ab74b1f2939bde53fc4149dd
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-04-27 16:10:51 -0400

    Prevent sending notification emails if the recipient is the same as the originator

--- src/AppServices.Core/AppNotifications/AppNotificationResult.cs
+++ src/AppServices.Core/AppNotifications/AppNotificationResult.cs
@@ -1,22 +1,20 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace AirWeb.AppServices.Core.AppNotifications;
+﻿namespace AirWeb.AppServices.Core.AppNotifications;
 
 public record AppNotificationResult
 {
-    private static readonly AppNotificationResult SucceededResult = new() { Succeeded = true };
+    private static readonly AppNotificationResult SucceededResult = new() { Succeeded = true, Attempted = true };
 
-    private static readonly AppNotificationResult NotAttemptedResult =
-        new() { FailureReason = "No notification email was sent." };
+    private static readonly AppNotificationResult NotAttemptedResult = new();
 
     private AppNotificationResult() { }
 
-    [MemberNotNullWhen(false, nameof(FailureReason))]
     public bool Succeeded { get; private init; }
-
+    public bool Attempted { get; private init; }
     public string? FailureReason { get; private init; }
 
     public static AppNotificationResult Success() => SucceededResult;
     public static AppNotificationResult NotAttempted() => NotAttemptedResult;
-    public static AppNotificationResult Failed(string failureMessage) => new() { FailureReason = failureMessage };
+
+    public static AppNotificationResult Failed(string failureMessage) =>
+        new() { Attempted = true, FailureReason = failureMessage };
 }
