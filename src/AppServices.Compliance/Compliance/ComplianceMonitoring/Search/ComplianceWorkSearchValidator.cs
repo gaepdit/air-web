131b3e654397f5f40d452814b1621b5a64932f9f
yoyocaleb <yoyocaleb1011@gmail.com>
2026-04-28 06:31:00 -0400

    Created Search Validator for Compliance Work

---
+++ src/AppServices.Compliance/Compliance/ComplianceMonitoring/Search/ComplianceWorkSearchValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using AirWeb.Domain.Compliance;
+using AirWeb.Domain.Compliance.ComplianceEntities.Fces;
+using FluentValidation;
+using IaipDataService.Facilities;
+
+namespace AirWeb.AppServices.Compliance.Compliance.ComplianceMonitoring.Search;
+
+public class ComplianceWorkValidator : AbstractValidator<ComplianceWorkSearchDto>
+{
+    private readonly IFacilityService _service;
+    public ComplianceWorkValidator(IFacilityService service)
+    {
+        _service = service;
+        var today = DateOnly.FromDateTime(DateTime.Today);
+
+        RuleFor(dto => dto.EventDateFrom)
+            .Must(date => date <= today || date == null)
+            .WithMessage("The Event From Date cannot be in the future.");
+    }
+}
