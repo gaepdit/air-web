8e641370282bb3af7a1e13f855c135795b921d8f
yoyocaleb <yoyocaleb1011@gmail.com>
2026-04-29 09:49:35 -0400

    created enforcement search validator file

---
+++ src/AppServices.Compliance/Enforcement/Search/CaseFileSearchValidator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirWeb.AppServices.Compliance.Enforcement.Search
+{
+    internal class CaseFileSearchValidator
+    {
+    }
+}
