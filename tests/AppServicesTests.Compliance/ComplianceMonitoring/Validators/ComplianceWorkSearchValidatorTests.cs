d19703e90f2c3f7990466268b3badde2074070f8
yoyocaleb <yoyocaleb1011@gmail.com>
2026-05-06 11:21:10 -0400

    created complianceworksearchvalidatortest file

---
+++ tests/AppServicesTests.Compliance/ComplianceMonitoring/Validators/ComplianceWorkSearchValidatorTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AirWeb.AppServices.Compliance.Compliance.ComplianceMonitoring.ComplianceWorkDto.Command;
+using AirWeb.AppServices.Compliance.Compliance.ComplianceMonitoring.Inspections;
+
+namespace AppServicesTests.Compliance.ComplianceMonitoring.Validators;
+
+public class ComplianceWorkSearchValidatorTests
+{
+    private static readonly ComplianceWorkCommandValidator ComplianceWorkCommandValidator = new();
+
+    private static readonly ComplianceWorkCreateValidator ComplianceWorkCreateValidator =
+        new(ComplianceWorkCommandValidator);
+
+}
+
