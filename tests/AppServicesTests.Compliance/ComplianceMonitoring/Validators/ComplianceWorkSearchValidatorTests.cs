c7e49335c27195551f0804a409115a4406571b94
yoyocaleb <yoyocaleb1011@gmail.com>
2026-05-07 10:54:32 -0400

    attempted to create test

--- tests/AppServicesTests.Compliance/ComplianceMonitoring/Validators/ComplianceWorkSearchValidatorTests.cs
+++ tests/AppServicesTests.Compliance/ComplianceMonitoring/Validators/ComplianceWorkSearchValidatorTests.cs
@@ -1,17 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using AirWeb.AppServices.Compliance.Compliance.ComplianceMonitoring.ComplianceWorkDto.Command;
-using AirWeb.AppServices.Compliance.Compliance.ComplianceMonitoring.Inspections;
+﻿using AirWeb.AppServices.Compliance.Compliance.ComplianceMonitoring.Search;
+using AirWeb.TestData.SampleData;
+using FluentValidation.TestHelper;
+using IaipDataService.Facilities;
 
-namespace AppServicesTests.Compliance.ComplianceMonitoring.Validators;
+namespace AppServicesTests.Compliance.ComplianceMonitoring.Search;
 
 public class ComplianceWorkSearchValidatorTests
 {
-    private static readonly ComplianceWorkCommandValidator ComplianceWorkCommandValidator = new();
+    private readonly IFacilityService _service;
 
-    private static readonly ComplianceWorkCreateValidator ComplianceWorkCreateValidator =
-        new(ComplianceWorkCommandValidator);
+    private static readonly ComplianceWorkValidator _validator = new(_service);
 
+    [Test]
+    public async Task EventDateFromInFuture_ReturnsAsInvalid()
+    {
+        // Arrange
+        var model = new ComplianceWorkSearchDto
+        {
+            FacilityId = SampleText.ValidFacilityId,
+            EventDateFrom = DateOnly.FromDateTime(DateTime.Today).AddDays(1),
+        };
+
+        // Act
+        var result = await _validator.TestValidateAsync(model);
+
+        // Assert
+        using var scope = new AssertionScope();
+        result.IsValid.Should().BeFalse();
+        result.ShouldHaveValidationErrorFor(dto => dto.EventDateFrom);
+    }
 }
-
