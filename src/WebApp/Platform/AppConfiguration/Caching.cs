009b3932811370d33217cd2b52da50fe4fe3df6d
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-05-11 14:22:06 -0400

    Add Hybrid Cache as a caching option

---
+++ src/WebApp/Platform/AppConfiguration/Caching.cs
@@ -0,0 +1,13 @@
+﻿using AirWeb.AppServices.Core.Caching;
+
+namespace AirWeb.WebApp.Platform.AppConfiguration;
+
+internal static class Caching
+{
+    public static IServiceCollection AddCaching(this IServiceCollection services)
+    {
+        services.AddHybridCache(options =>
+            options.DefaultEntryOptions = CacheUtilities.GetHybridCacheOptions(TimeSpan.FromDays(1)));
+        return services;
+    }
+}
