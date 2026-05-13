12671ed22922bb00a27bbaf1ed008aecf9aeccc0
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-05-13 16:39:45 -0400

    Update location hash instead of location href

--- src/WebApp/wwwroot/js/maps/mapAllInit.js
+++ src/WebApp/wwwroot/js/maps/mapAllInit.js
@@ -5,14 +5,14 @@ function initMap() {
         detectRetina: true,
         className: 'map-tiles',
     });
-    const map = L.map('facility-map', { center: [32.9, -83.3], zoom: 7, layers: [tiles] });
+    const map = L.map('facility-map', {center: [32.9, -83.3], zoom: 7, layers: [tiles]});
 
     // Fix the marker icon path.
     L.Icon.Default.prototype.options.imagePath = '/lib/leaflet/dist/images/';
 
     map.whenReady(function () {
         // Add a location control.
-        L.control.locate({ initialZoomLevel: 13, onLocationError: showAlert }).addTo(map);
+        L.control.locate({initialZoomLevel: 13, onLocationError: showAlert}).addTo(map);
         map.on('locateactivate', removeAlert);
 
         // Enable permalinks.
@@ -26,7 +26,7 @@ function initMap() {
         // Add markers for facilities with geocoordinates.
         const markers = L.markerClusterGroup({
             chunkedLoading: true,
-            spiderLegPolylineOptions: { weight: 2, color: '#1d78c9', opacity: 0.5 },
+            spiderLegPolylineOptions: {weight: 2, color: '#1d78c9', opacity: 0.5},
         });
         for (const f of facilities) if (f.GeoCoordinates) markers.addLayer(newMarker(f));
         map.addLayer(markers);
@@ -64,7 +64,7 @@ function newMarker(f) {
         `<div>${f.Location}</div>` +
         `<div class="facility-map-link"><a target="_blank" href="Details/${f.Id}">${f.Id}</a></div>` +
         '</div>';
-    const marker = L.marker([f.GeoCoordinates.Latitude, f.GeoCoordinates.Longitude], { title: f.Name });
+    const marker = L.marker([f.GeoCoordinates.Latitude, f.GeoCoordinates.Longitude], {title: f.Name});
     marker.bindPopup(info);
     return marker;
 }
@@ -75,7 +75,7 @@ function initCitySelector() {
     const citySelector = document.getElementById('city-selector');
     citySelector.addEventListener('change', function () {
         const selectedView = citySelector.value;
-        if (selectedView) window.location.href = selectedView;
+        if (selectedView) window.location.hash = selectedView;
     });
 }
 
