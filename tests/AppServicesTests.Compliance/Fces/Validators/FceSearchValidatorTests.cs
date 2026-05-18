6a571cf1076bbd7a2b84ff143eb3b45fe3dd0f80
yoyocaleb <yoyocaleb1011@gmail.com>
2026-05-18 09:28:56 -0400

    SetUp created for Fce Search

--- tests/AppServicesTests.Compliance/Fces/Validators/FceSearchValidatorTests.cs
+++ tests/AppServicesTests.Compliance/Fces/Validators/FceSearchValidatorTests.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using AirWeb.AppServices.Compliance.Compliance.Fces.Search;
+using IaipDataService.Facilities;
 
-namespace AppServicesTests.Compliance.Fces.Validators
+namespace AppServicesTests.Compliance.Fces.Validators;
+
+public class FceSearchValidatorTests
 {
-    internal class FceSearchValidatorTests
+    private IFacilityService _service = null!;
+    private FceSearchValidator _sut;
+    private IFacilityService _serviceFalse = null!;
+    private FceSearchValidator _sutFalse;
+
+    [SetUp]
+    public void SetUp()
     {
+        // Arrange
+        _service = Substitute.For<IFacilityService>();
+        _service.ExistsAsync(Arg.Any<FacilityId>())
+            .Returns(true);
+        _sut = new FceSearchValidator(_service);
+
+        _serviceFalse = Substitute.For<IFacilityService>();
+        _service.ExistsAsync(Arg.Any<FacilityId>())
+            .Returns(false);
+        _sut = new FceSearchValidator( _serviceFalse);
     }
+
+
 }
