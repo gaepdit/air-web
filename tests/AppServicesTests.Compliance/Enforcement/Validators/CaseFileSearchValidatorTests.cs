72cca8ac76edf50fae7b7faf7713fd1bf27bc1df
yoyocaleb <yoyocaleb1011@gmail.com>
2026-05-20 11:25:42 -0400

    CaseFileSearchValidatorTests finished

--- tests/AppServicesTests.Compliance/Enforcement/Validators/CaseFileSearchValidatorTests.cs
+++ tests/AppServicesTests.Compliance/Enforcement/Validators/CaseFileSearchValidatorTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using AirWeb.AppServices.Compliance.Compliance.ComplianceMonitoring.Search;
 using AirWeb.AppServices.Compliance.Compliance.Fces.Search;
 using AirWeb.AppServices.Compliance.Enforcement.Search;
 using AirWeb.TestData.SampleData;
@@ -47,5 +48,133 @@ public class CaseFileSearchValidatorTests
 
         // Assert
         result.IsValid.Should().BeTrue();
+    }
+
+    [Test]
+    public async Task DiscoveryDateFromInFuture_ReturnsAsInvalid()
+    {
+        // Arrange
+        var model = new CaseFileSearchDto
+        {
+            DiscoveryDateFrom = DateOnly.FromDateTime(DateTime.Today).AddDays(1),
+        };
+
+        // Act
+        var result = await _sut.TestValidateAsync(model);
+
+        // Assert
+        using var scope = new AssertionScope();
+        result.IsValid.Should().BeFalse();
+        result.ShouldHaveValidationErrorFor(dto => dto.DiscoveryDateFrom);
+    }
+
+    [Test]
+    public async Task DiscoveryDateToInFuture_ReturnsAsInvalid()
+    {
+        // Arrange
+        var model = new CaseFileSearchDto
+        {
+            DiscoveryDateTo = DateOnly.FromDateTime(DateTime.Today).AddDays(1)
+        };
+
+        // Act
+        var result = await _sut.TestValidateAsync(model);
+
+        // Assert
+        using var scope = new AssertionScope();
+        result.IsValid.Should().BeFalse();
+        result.ShouldHaveValidationErrorFor(dto => dto.DiscoveryDateTo);
+    }
+
+    [Test]
+    public async Task DiscoveryDateToIsBeforeDiscoveryDateFrom_ReturnsAsInvalid()
+    {
+        // Arrange
+        var model = new CaseFileSearchDto
+        {
+            DiscoveryDateFrom = DateOnly.FromDateTime(DateTime.Today).AddDays(-1),
+            DiscoveryDateTo = DateOnly.FromDateTime(DateTime.Today).AddDays(-5)
+        };
+
+        // Act
+        var results = await _sut.TestValidateAsync(model);
+
+        // Assert
+        using var scope = new AssertionScope();
+        results.IsValid.Should().BeFalse();
+        results.ShouldHaveValidationErrorFor(dto => dto.DiscoveryDateTo);
+    }
+
+    [Test]
+    public async Task EnforcementDateFromInFuture_ReturnsAsInvalid()
+    {
+        // Arrange
+        var model = new CaseFileSearchDto
+        {
+            EnforcementDateFrom = DateOnly.FromDateTime(DateTime.Today).AddDays(1),
+        };
+
+        // Act
+        var result = await _sut.TestValidateAsync(model);
+
+        // Assert
+        using var scope = new AssertionScope();
+        result.IsValid.Should().BeFalse();
+        result.ShouldHaveValidationErrorFor(dto => dto.EnforcementDateFrom);
+    }
+
+    [Test]
+    public async Task EnforcementDateToInFuture_ReturnsAsInvalid()
+    {
+        // Arrange
+        var model = new CaseFileSearchDto
+        {
+            EnforcementDateTo = DateOnly.FromDateTime(DateTime.Today).AddDays(1)
+        };
+
+        // Act
+        var result = await _sut.TestValidateAsync(model);
+
+        // Assert
+        using var scope = new AssertionScope();
+        result.IsValid.Should().BeFalse();
+        result.ShouldHaveValidationErrorFor(dto => dto.EnforcementDateTo);
+    }
+
+    [Test]
+    public async Task EnforcementDateToIsBeforeEnforcementDateFrom_ReturnsAsInvalid()
+    {
+        // Arrange
+        var model = new CaseFileSearchDto
+        {
+            EnforcementDateFrom = DateOnly.FromDateTime(DateTime.Today).AddDays(-1),
+            EnforcementDateTo = DateOnly.FromDateTime(DateTime.Today).AddDays(-5)
+        };
+
+        // Act
+        var results = await _sut.TestValidateAsync(model);
+
+        // Assert
+        using var scope = new AssertionScope();
+        results.IsValid.Should().BeFalse();
+        results.ShouldHaveValidationErrorFor(dto => dto.EnforcementDateTo);
+    }
+
+    [Test]
+    public async Task FacilityIdDoesNotExist_ReturnsAsInvalid()
+    {
+        // Arrange
+        var model = new CaseFileSearchDto
+        {
+            FacilityId = "00999999",
+        };
+
+        // Act
+        var results = await _sutFalse.TestValidateAsync(model);
+
+        // Assert
+        using var scope = new AssertionScope();
+        results.IsValid.Should().BeFalse();
+        results.ShouldHaveValidationErrorFor(dto => dto.FacilityId);
     }
 }
