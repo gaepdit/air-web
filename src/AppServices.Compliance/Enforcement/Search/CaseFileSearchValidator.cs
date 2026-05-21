44ac50815950d5c1d065186198af0205cdd599bf
yoyocaleb <yoyocaleb1011@gmail.com>
2026-05-21 12:07:58 -0400

    changed rule for facilityId to have two validations to see if id is null or valid format and for async

--- src/AppServices.Compliance/Enforcement/Search/CaseFileSearchValidator.cs
+++ src/AppServices.Compliance/Enforcement/Search/CaseFileSearchValidator.cs
@@ -37,9 +37,20 @@ public class CaseFileSearchValidator : AbstractValidator<CaseFileSearchDto>
             .WithMessage("The Enforcement To Date must be later than the From Date");
 
         RuleFor(dto => dto.FacilityId)
-           .MustAsync(async (id, cancellation) =>
-               await _service.ExistsAsync((FacilityId)id!))
-           .WithMessage("A Facility with that AIRS Number does not exist or has not been approved in the IAIP.")
-           .When(dto => !string.IsNullOrWhiteSpace(dto.FacilityId));
+            .Cascade(CascadeMode.Stop)
+            .Must(id => string.IsNullOrWhiteSpace(id) ||
+                FacilityId.TryParse(id, out _))
+            .WithMessage(FacilityId.FacilityIdFormatError)
+            .MustAsync(async (id, cancellation) =>
+            {
+                if (string.IsNullOrWhiteSpace(id))
+                    return true;
+
+                if (!FacilityId.TryParse(id, out var facilityId))
+                    return false;
+
+                return await _service.ExistsAsync(facilityId);
+            })
+            .WithMessage("A Facility with that AIRS Number does not exist or has not been approved in the IAIP.");
     }
 }
