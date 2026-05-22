d564ba5925b2939909e88efe76616fa95a80b75e
Doug Waldron <737326+dougwaldron@users.noreply.github.com>
2026-05-22 11:03:14 -0400

    Add EF migration for updated Enf Action Review date data types (#616)

---
+++ src/EfRepository/Migrations/20260522144509_EnfReviewDateTypes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AirWeb.EfRepository.Migrations
+{
+    /// <inheritdoc />
+    public partial class EnfReviewDateTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "ReviewRequestedDate",
+                table: "EnforcementActions",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "RequestedDate",
+                table: "EnforcementActionReviews",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ReviewRequestedDate",
+                table: "EnforcementActions",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RequestedDate",
+                table: "EnforcementActionReviews",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+    }
+}
