af17c94487b12ab34440d9db9c4738312f2d1928
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-06-02 16:36:04 -0400

    Refactor Facility summary serialization.

--- src/IaipDataService/Structs/GeoCoordinates.cs
+++ src/IaipDataService/Structs/GeoCoordinates.cs
@@ -2,6 +2,6 @@
 
 public readonly record struct GeoCoordinates(decimal Latitude, decimal Longitude)
 {
-    public string GoogleMapsUrl => $"https://www.google.com/maps/search/?api=1&query={Latitude}%2C{Longitude}";
-    public string AppleMapsUrl => $"https://maps.apple.com/?q={Latitude}%2C{Longitude}";
+    public string GoogleMapsUrl() => $"https://www.google.com/maps/search/?api=1&query={Latitude}%2C{Longitude}";
+    public string AppleMapsUrl() => $"https://maps.apple.com/?q={Latitude}%2C{Longitude}";
 }
