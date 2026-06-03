b4da1aa6f2733529ee0ac6197d996994e91297ff
yoyocaleb <yoyocaleb1011@gmail.com>
2026-06-03 11:43:25 -0400

    various changes to ComplianceWorkSearchValidator

--- src/AppServices.Compliance/Compliance/ComplianceMonitoring/Search/ComplianceWorkSearchValidator.cs
+++ src/AppServices.Compliance/Compliance/ComplianceMonitoring/Search/ComplianceWorkSearchValidator.cs
@@ -1,16 +1,12 @@
-﻿using AirWeb.Domain.Compliance;
-using AirWeb.Domain.Compliance.ComplianceEntities.Fces;
-using FluentValidation;
+﻿using FluentValidation;
 using IaipDataService.Facilities;
 
 namespace AirWeb.AppServices.Compliance.Compliance.ComplianceMonitoring.Search;
 
-public class ComplianceWorkValidator : AbstractValidator<ComplianceWorkSearchDto>
+public class ComplianceWorkSearchValidator : AbstractValidator<ComplianceWorkSearchDto>
 {
-    private readonly IFacilityService _service;
-    public ComplianceWorkValidator(IFacilityService service)
+    public ComplianceWorkSearchValidator(IFacilityService service)
     {
-        _service = service;
         var today = DateOnly.FromDateTime(DateTime.Today);
 
         RuleFor(dto => dto.EventDateFrom)
@@ -18,10 +14,8 @@ public class ComplianceWorkValidator : AbstractValidator<ComplianceWorkSearchDto
             .WithMessage("The Event From Date cannot be in the future.");
 
         RuleFor(dto => dto.EventDateTo)
-            .Cascade(CascadeMode.Stop)
-            .Must(date => date <= today || date == null)
-            .WithMessage("The Event To Date cannot be in the future")
-            .Must((dto, date) => dto.EventDateFrom == default || date >= dto.EventDateFrom || date == null)
+            .Must((dto, date) => date >= dto.EventDateFrom)
+            .When(dto => dto.EventDateFrom.HasValue && dto.EventDateTo.HasValue)
             .WithMessage("The Event To Date must be later than the From Date");
 
         RuleFor(dto => dto.ClosedDateFrom)
@@ -29,10 +23,8 @@ public class ComplianceWorkValidator : AbstractValidator<ComplianceWorkSearchDto
             .WithMessage("The Closed From Date cannot be in the future");
 
         RuleFor(dto => dto.ClosedDateTo)
-            .Cascade(CascadeMode.Stop)
-            .Must(date => date <= today || date == null)
-            .WithMessage("The Closed To Date cannot be in the future")
-            .Must((dto, date) => dto.ClosedDateFrom == default || date >= dto.ClosedDateFrom || date == null)
+            .Must((dto, date) => date >= dto.ClosedDateFrom)
+            .When(dto => dto.ClosedDateFrom.HasValue && dto.ClosedDateTo.HasValue)
             .WithMessage("The Closed To Date must be later than the From Date");
 
         RuleFor(dto => dto.FacilityId)
@@ -48,7 +40,7 @@ public class ComplianceWorkValidator : AbstractValidator<ComplianceWorkSearchDto
                 if (!FacilityId.TryParse(id, out var facilityId))
                     return false;
 
-                return await _service.ExistsAsync(facilityId).ConfigureAwait(false);
+                return await service.ExistsAsync(facilityId).ConfigureAwait(false);
             })
             .WithMessage("A Facility with that AIRS Number does not exist or has not been approved in the IAIP.");
     }
