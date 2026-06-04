76559b08f2a3dda5d03b4f5f264fa30c7a2a36e3
yoyocaleb <yoyocaleb1011@gmail.com>
2026-06-04 10:39:39 -0400

    various changes in FceSearchValidator

--- src/AppServices.Compliance/Compliance/Fces/Search/FceSearchValidator.cs
+++ src/AppServices.Compliance/Compliance/Fces/Search/FceSearchValidator.cs
@@ -1,16 +1,12 @@
-﻿using AirWeb.Domain.Compliance;
-using AirWeb.Domain.Compliance.ComplianceEntities.Fces;
-using FluentValidation;
+﻿using FluentValidation;
 using IaipDataService.Facilities;
 
 namespace AirWeb.AppServices.Compliance.Compliance.Fces.Search;
 
 public class FceSearchValidator : AbstractValidator<FceSearchDto>
 {
-    private readonly IFacilityService _service;
     public FceSearchValidator(IFacilityService service)
     {
-        _service = service;
         var today = DateOnly.FromDateTime(DateTime.Today);
 
         RuleFor(dto => dto.DateFrom)
@@ -18,10 +14,8 @@ public class FceSearchValidator : AbstractValidator<FceSearchDto>
             .WithMessage("The FCE From Date cannot be in the future.");
 
         RuleFor(dto => dto.DateTo)
-            .Cascade(CascadeMode.Stop)
-            .Must(date => date <= today || date == null)
-            .WithMessage("The FCE To Date cannot be in the future.")
-            .Must((dto, date) => dto.DateFrom == default || date >= dto.DateFrom || date == null)
+            .Must((dto, date) => date >= dto.DateFrom)
+            .When(dto => dto.DateFrom.HasValue && dto.DateTo.HasValue)
             .WithMessage("The FCE To Date must be later than the From Date.");
 
         RuleFor(dto => dto.FacilityId)
@@ -37,7 +31,7 @@ public class FceSearchValidator : AbstractValidator<FceSearchDto>
                 if (!FacilityId.TryParse(id, out var facilityId))
                     return false;
 
-                return await _service.ExistsAsync(facilityId).ConfigureAwait(false);
+                return await service.ExistsAsync(facilityId).ConfigureAwait(false);
             })
             .WithMessage("A Facility with that AIRS Number does not exist or has not been approved in the IAIP.");
     }
