76640c56961c120c1f28bb5c6cbec6320fe36242
yoyocaleb <yoyocaleb1011@gmail.com>
2026-06-18 10:02:40 -0400

    created casesearch validator with a test (non funtional) added iaipdataservice reference

---
+++ src/AppServices.Sbeap/Cases/Validators/CaseSearchValidator.cs
@@ -0,0 +1,22 @@
+﻿using IaipDataService.Facilities;
+using AirWeb.AppServices.Sbeap.Cases.Dto;
+using FluentValidation;
+
+namespace AirWeb.AppServices.Sbeap.Cases.Validators;
+
+public class CaseSearchValidator : AbstractValidator<CaseworkSearchDto>
+{
+    public CaseSearchValidator(IFacilityService service)
+    {
+        var today = DateOnly.FromDateTime(DateTime.Today);
+
+        RuleFor(dto => dto.ReferredFrom)
+            .Must(date => date <= today || date == null)
+            .WithMessage("The beginning referred date cannot be in the future");
+
+        RuleFor(dto => dto.ReferredThrough)
+            .Must((dto, date) => date >= dto.ReferredFrom)
+            .When(dto => dto.ReferredFrom.HasValue && dto.ReferredThrough.HasValue)
+            .WithMessage("The end referred date must be later than the beginning date");
+    }
+}
