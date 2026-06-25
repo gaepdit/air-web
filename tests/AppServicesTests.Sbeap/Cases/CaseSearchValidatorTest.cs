b1d7d127d547ce6904e03ff0b1bb2009dd444c5a
yoyocaleb <yoyocaleb1011@gmail.com>
2026-06-25 08:53:14 -0400

    moved casesearchvalidatortest to sbeap test. removed unused reference

---
+++ tests/AppServicesTests.Sbeap/Cases/CaseSearchValidatorTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AirWeb.AppServices.Sbeap.Cases.Validators;
+using IaipDataService.Facilities;
+
+namespace AppServicesTests.Compliance.Enforcement.Validators;
+
+public class CaseSearchValidatorTests
+{
+    private CaseSearchValidator _sut;
+}
