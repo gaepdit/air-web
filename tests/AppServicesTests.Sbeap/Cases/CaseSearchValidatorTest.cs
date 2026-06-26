59a9e491fba5175a75bf8b927675e0cee6ff3564
yoyocaleb <yoyocaleb1011@gmail.com>
2026-06-26 10:56:02 -0400

    unit test created and pass

--- tests/AppServicesTests.Sbeap/Cases/CaseSearchValidatorTest.cs
+++ tests/AppServicesTests.Sbeap/Cases/CaseSearchValidatorTest.cs
@@ -1,6 +1,7 @@
-﻿using FluentValidation.TestHelper;
-using AirWeb.AppServices.Sbeap.Cases.Dto;
+﻿using AirWeb.AppServices.Sbeap.Cases.Dto;
 using AirWeb.AppServices.Sbeap.Cases.Validators;
+using AwesomeAssertions.Execution;
+using FluentValidation.TestHelper;
 
 namespace AppServicesTests.Compliance.Enforcement.Validators;
 
@@ -11,7 +12,7 @@ public class CaseSearchValidatorTests
     [SetUp]
     public void SetUp()
     {
-        // Arrange           
+        // Arrange
         _sut = new CaseSearchValidator();
     }
     [Test]
@@ -47,4 +48,113 @@ public class CaseSearchValidatorTests
         // Assert
         result.IsValid.Should().BeTrue();
     }
+    [Test]
+    public async Task OpenedFromInFuture_ReturnsAsInvalid()
+    {
+        // Arrange
+        var model = new CaseworkSearchDto
+        {
+            OpenedFrom = DateOnly.FromDateTime(DateTime.Today).AddDays(1),
+        };
+
+        // Act
+        var result = await _sut.TestValidateAsync(model);
+
+        // Assert
+        using var scope = new AssertionScope();
+        result.IsValid.Should().BeFalse();
+        result.ShouldHaveValidationErrorFor(dto => dto.OpenedFrom);
+    }
+
+    [Test]
+    public async Task OpenedThroughIsBeforeOpenedFrom_ReturnsAsInvalid()
+    {
+        // Arrange
+        var model = new CaseworkSearchDto
+        {
+            OpenedFrom = DateOnly.FromDateTime(DateTime.Today).AddDays(-1),
+            OpenedThrough = DateOnly.FromDateTime(DateTime.Today).AddDays(-5)
+        };
+
+        // Act
+        var results = await _sut.TestValidateAsync(model);
+
+        // Assert
+        using var scope = new AssertionScope();
+        results.IsValid.Should().BeFalse();
+        results.ShouldHaveValidationErrorFor(dto => dto.OpenedThrough);
+    }
+    [Test]
+    public async Task ClosedFromInFuture_ReturnsAsInvalid()
+    {
+        // Arrange
+        var model = new CaseworkSearchDto
+        {
+            ClosedFrom = DateOnly.FromDateTime(DateTime.Today).AddDays(1),
+        };
+
+        // Act
+        var result = await _sut.TestValidateAsync(model);
+
+        // Assert
+        using var scope = new AssertionScope();
+        result.IsValid.Should().BeFalse();
+        result.ShouldHaveValidationErrorFor(dto => dto.ClosedFrom);
+    }
+
+    [Test]
+    public async Task ClosedThroughIsBeforeClosedFrom_ReturnsAsInvalid()
+    {
+        // Arrange
+        var model = new CaseworkSearchDto
+        {
+            ClosedFrom = DateOnly.FromDateTime(DateTime.Today).AddDays(-1),
+            ClosedThrough = DateOnly.FromDateTime(DateTime.Today).AddDays(-5)
+        };
+
+        // Act
+        var results = await _sut.TestValidateAsync(model);
+
+        // Assert
+        using var scope = new AssertionScope();
+        results.IsValid.Should().BeFalse();
+        results.ShouldHaveValidationErrorFor(dto => dto.ClosedThrough);
+    }
+    [Test]
+    public async Task ReferredFromInFuture_ReturnsAsInvalid()
+    {
+        // Arrange
+        var model = new CaseworkSearchDto
+        {
+            ReferredFrom = DateOnly.FromDateTime(DateTime.Today).AddDays(1),
+        };
+
+        // Act
+        var result = await _sut.TestValidateAsync(model);
+
+        // Assert
+        using var scope = new AssertionScope();
+        result.IsValid.Should().BeFalse();
+        result.ShouldHaveValidationErrorFor(dto => dto.ReferredFrom);
+    }
+
+    [Test]
+    public async Task ReferredThroughIsBeforeReferredFrom_ReturnsAsInvalid()
+    {
+        // Arrange
+        var model = new CaseworkSearchDto
+        {
+            ReferredFrom = DateOnly.FromDateTime(DateTime.Today).AddDays(-1),
+            ReferredThrough = DateOnly.FromDateTime(DateTime.Today).AddDays(-5)
+        };
+
+        // Act
+        var results = await _sut.TestValidateAsync(model);
+
+        // Assert
+        using var scope = new AssertionScope();
+        results.IsValid.Should().BeFalse();
+        results.ShouldHaveValidationErrorFor(dto => dto.ReferredThrough);
+    }
+
 }
