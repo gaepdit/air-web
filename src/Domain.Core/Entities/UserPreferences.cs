a7e33084a35847dd394a0de1c038db892d8817c0
yoyocaleb <yoyocaleb1011@gmail.com>
2026-07-16 10:42:00 -0400

    UserPreferences created

---
+++ src/Domain.Core/Entities/UserPreferences.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirWeb.Domain.Core.Entities;
+
+public class UserPreferences
+{
+    public ThemePreference Theme { get; set; } = ThemePreference.Auto;
+}
+public enum ThemePreference
+{
+    Auto = 0,
+    Light = 1,
+    Dark = 2,
+}
