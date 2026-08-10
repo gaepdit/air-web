gaepdit/air-web

commit 68b2f452770c15b21987649dda4d30a3f96e2bb7

Enable creating linked compliance work directly from enforcement (#673)

src/AppServices.Compliance/Compliance/ComplianceMonitoring/Notifications/NotificationCreateDto.cs
@@ -5,6 +5,7 @@ namespace AirWeb.AppServices.Compliance.Compliance.ComplianceMonitoring.Notifica
 public record NotificationCreateDto : NotificationCommandDto, IComplianceWorkCreateDto
 {
     [Required]
-    [Display(Name = "Facility")]
-    public string? FacilityId { get; init; }
+    public string? FacilityId { get; set; }
+
+    public int? CaseFileId => null;
 }

src/AppServices.Compliance/Compliance/ComplianceMonitoring/PermitRevocations/PermitRevocationCreateDto.cs
@@ -5,6 +5,7 @@ namespace AirWeb.AppServices.Compliance.Compliance.ComplianceMonitoring.PermitRe
 public record PermitRevocationCreateDto : PermitRevocationCommandDto, IComplianceWorkCreateDto
 {
     [Required]
-    [Display(Name = "Facility")]
-    public string? FacilityId { get; init; }
+    public string? FacilityId { get; set; }
+
+    public int? CaseFileId => null;
 }

src/AppServices.Compliance/Compliance/ComplianceMonitoring/Accs/AccCreateDto.cs
@@ -4,7 +4,6 @@ namespace AirWeb.AppServices.Compliance.Compliance.ComplianceMonitoring.Accs;
 
 public record AccCreateDto : AccCommandDto, IComplianceWorkCreateDto
 {
-    [Required]
-    [Display(Name = "Facility")]
-    public string? FacilityId { get; init; }
+    public string? FacilityId { get; set; }
+    public int? CaseFileId { get; init; }
 }

src/AppServices.Compliance/Compliance/ComplianceMonitoring/ComplianceWorkDto/Command/ComplianceWorkCreateValidator.cs
@@ -4,7 +4,7 @@ public class ComplianceWorkCreateValidator : AbstractValidator<IComplianceWorkCr
 {
     public ComplianceWorkCreateValidator(IValidator<IComplianceWorkCommandDto> complianceWorkCommandValidator)
     {
-        RuleFor(dto => dto.FacilityId).NotEmpty();
+        RuleFor(dto => dto).Must(dto => dto.FacilityId != null || dto.CaseFileId != null);
         RuleFor(dto => dto).SetValidator(complianceWorkCommandValidator);
     }
 }

src/AppServices.Compliance/Compliance/ComplianceMonitoring/ComplianceWorkDto/Command/IComplianceWorkCreateDto.cs
@@ -2,5 +2,6 @@
 
 public interface IComplianceWorkCreateDto : IComplianceWorkCommandDto
 {
-    public string? FacilityId { get; }
+    public string? FacilityId { get; set; }
+    public int? CaseFileId { get; }
 }

src/AppServices.Compliance/Compliance/ComplianceMonitoring/ComplianceWorkService.cs
@@ -17,6 +17,8 @@ using AirWeb.Domain.Compliance.ComplianceEntities.ComplianceMonitoring;
 using AutoMapper;
 using IaipDataService.Facilities;
 using IaipDataService.SourceTests;
+using Microsoft.Identity.Web;
+using System.Security.Claims;
 
 namespace AirWeb.AppServices.Compliance.Compliance.ComplianceMonitoring;
 
@@ -98,11 +100,25 @@ public sealed partial class ComplianceWorkService(
             .ConfigureAwait(false));
 
     // Command
-    public async Task<CreateResult<int>> CreateAsync(IComplianceWorkCreateDto resource,
+    public async Task<CreateResult<int>> CreateAsync(IComplianceWorkCreateDto resource, ClaimsPrincipal principal,
         CancellationToken token = default)
     {
-        var currentUser = await userService.GetCurrentUserAsync().ConfigureAwait(false);
-        var work = await CreateComplianceWorkFromDtoAsync(resource, currentUser, token).ConfigureAwait(false);
+        if (resource.FacilityId is null && resource.CaseFileId is null ||
+            (resource.FacilityId is not null && resource.CaseFileId is not null))
+            throw new InvalidOperationException();
+
+        var user = await userService.GetUserAsync(principal.GetNameIdentifierId() ?? throw new
+            InvalidOperationException()).ConfigureAwait(false);
+
+        if (resource.CaseFileId is not null)
+        {
+            var caseFile = await caseFileService.FindSummaryAsync(resource.CaseFileId.Value, token)
+                .ConfigureAwait(false);
+            if (caseFile is null) throw new InvalidOperationException();
+            resource.FacilityId = caseFile.FacilityId;
+        }
+
+        var work = await CreateComplianceWorkFromDtoAsync(resource, user, token).ConfigureAwait(false);
         await repository.InsertAsync(work, token: token).ConfigureAwait(false);
 
         if (work is SourceTestReview str)
@@ -115,7 +131,12 @@ public sealed partial class ComplianceWorkService(
 
         var notificationResult = await appNotificationService
             .SendNotificationAsync(ComplianceTemplate.WorkCreated, work.ResponsibleStaff, token,
-                work.Id, work.FacilityId, currentUser?.FullName).ConfigureAwait(false);
+                work.Id, work.FacilityId, user.FullName).ConfigureAwait(false);
+
+        if (resource.CaseFileId is not null)
+            await caseFileService.LinkComplianceEventAsync(resource.CaseFileId.Value, work.Id, token)
+                .ConfigureAwait(false);
+
         return CreateResult<int>.Create(work.Id, notificationResult.FailureReason);
     }
 

src/AppServices.Compliance/Compliance/ComplianceMonitoring/IComplianceWorkService.cs
@@ -4,6 +4,7 @@ using AirWeb.AppServices.Compliance.Compliance.ComplianceMonitoring.SourceTestRe
 using AirWeb.AppServices.Core.CommonDtos;
 using AirWeb.AppServices.Core.EntityServices.Comments;
 using AirWeb.Domain.Compliance.ComplianceEntities.ComplianceMonitoring;
+using System.Security.Claims;
 
 namespace AirWeb.AppServices.Compliance.Compliance.ComplianceMonitoring;
 
@@ -23,7 +24,9 @@ public interface IComplianceWorkService : IDisposable, IAsyncDisposable
     Task<SourceTestReviewViewDto?> FindSourceTestReviewAsync(int referenceNumber, CancellationToken token = default);
 
     // Command
-    Task<CreateResult<int>> CreateAsync(IComplianceWorkCreateDto resource, CancellationToken token = default);
+    Task<CreateResult<int>> CreateAsync(IComplianceWorkCreateDto resource, ClaimsPrincipal principal,
+        CancellationToken token = default);
+
     Task<CommandResult> UpdateAsync(int id, IComplianceWorkCommandDto resource, CancellationToken token = default);
     Task<CommandResult> CloseAsync(int id, CancellationToken token = default);
     Task<CommandResult> ReopenAsync(int id, CancellationToken token = default);

src/AppServices.Compliance/Compliance/ComplianceMonitoring/Inspections/InspectionCreateDto.cs
@@ -4,9 +4,7 @@ namespace AirWeb.AppServices.Compliance.Compliance.ComplianceMonitoring.Inspecti
 
 public record InspectionCreateDto : InspectionCommandDto, IComplianceWorkCreateDto
 {
-    [Required]
-    [Display(Name = "Facility")]
-    public string? FacilityId { get; init; }
-
+    public string? FacilityId { get; set; }
+    public int? CaseFileId { get; init; }
     public bool IsRmpInspection { get; init; }
 }

commit 79f567f813667d1ce2d1c0f0578a475275197398

Fix bug preventing new notification or permit revocation
Bug introduced in 68b2f452

src/AppServices.Compliance/Compliance/ComplianceMonitoring/Notifications/NotificationCreateDto.cs
@@ -4,8 +4,6 @@ namespace AirWeb.AppServices.Compliance.Compliance.ComplianceMonitoring.Notifica
 
 public record NotificationCreateDto : NotificationCommandDto, IComplianceWorkCreateDto
 {
-    [Required]
     public string? FacilityId { get; set; }
-
     public int? CaseFileId => null;
 }

src/AppServices.Compliance/Compliance/ComplianceMonitoring/PermitRevocations/PermitRevocationCreateDto.cs
@@ -4,8 +4,6 @@ namespace AirWeb.AppServices.Compliance.Compliance.ComplianceMonitoring.PermitRe
 
 public record PermitRevocationCreateDto : PermitRevocationCommandDto, IComplianceWorkCreateDto
 {
-    [Required]
     public string? FacilityId { get; set; }
-
     public int? CaseFileId => null;
 }

src/WebApp/Pages/Compliance/SourceTest/Details.cshtml
@@ -155,7 +155,7 @@
                     <div>
                         @if (Model.UserCan[ComplianceOperation.Edit])
                         {
-                            <a asp-page="../Work/Edit/Index" asp-route-id="@Model.ComplianceReview.Id.ToString()"
+                            <a asp-page="../Work/Edit/Index" asp-route-id="@review.Id.ToString()"
                                class="btn btn-outline-primary me-1">Edit</a>
                         }
                         @if (Model.UserCan[ComplianceOperation.Close] || Model.UserCan[ComplianceOperation.Reopen])
@@ -191,7 +191,7 @@
                 <dd class="col-sm-8 col-lg-9">@Html.DisplayFor(_ => review.ResponsibleStaff, DisplayTemplate.NameOrPlaceholder)</dd>
                 <dt class="col-sm-4 col-lg-3">Compliance Review Status</dt>
                 <dd class="col-sm-8 col-lg-9">
-                    @if (Model.ComplianceReview.IsClosed)
+                    @if (review.IsClosed)
                     {
                         @:Closed
                         if (review.ClosedBy is not null)