e75986e03fe86b47d601c6cb2e9df6b01efd57e6
yoyocaleb <yoyocaleb1011@gmail.com>
2026-09-02 10:11:30 -0400

    created facilitycomment

---
+++ src/Domain.Compliance/Comments/FacilityComment.cs
@@ -0,0 +1,23 @@
+﻿using AirWeb.Domain.Core.Entities;
+
+namespace AirWeb.Domain.Compliance.Comments;
+
+public record FacilityComment : Comment, ISetCommentItemId<FacilityId>
+{
+    public FacilityComment() { }
+
+    // This constructor is only used for creating test data.
+    public FacilityComment(Comment comment, FacilityId facilityId)
+    {
+        Id = Guid.NewGuid();
+        Text = comment.Text;
+        CommentBy = comment.CommentBy;
+        CommentedAt = comment.CommentedAt;
+        FacilityId = facilityId;
+    }
+
+    public FacilityId FacilityId { get; private set; }
+    public void SetItemId(FacilityId facilityId) => FacilityId = facilityId;
+}
+
+public interface IFacilityCommentRepository : ICommentRepository<CaseFileComment>;
