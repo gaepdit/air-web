43c8058632d1678795e0e7bf7fee59103a8a2382
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-09-08 17:25:21 -0400

    Add a permit search form with autocomplete fields

---
+++ src/WebApp/Pages/PermitSearch/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using AirWeb.WebApp.Platform.Settings;
+using IaipDataService.Facilities;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+
+namespace AirWeb.WebApp.Pages.PermitSearch;
+
+public class PermitSearchIndex(IFacilityService service) : PageModel
+{
+    [Display(Name = "Facility ID/AIRS Number")]
+    public string? Id { get; set; }
+
+    [Display(Name = "Facility Name")]
+    public string? Name { get; set; }
+
+    public string FacilitiesAsJson => JsonSerializer.Serialize(Facilities, SerializationDefaults.Options);
+    public IReadOnlyCollection<FacilityList> Facilities { get; private set; } = null!;
+
+    public async Task OnGetAsync(CancellationToken token = default) =>
+        Facilities = await service.GetListAsync(token);
+}
