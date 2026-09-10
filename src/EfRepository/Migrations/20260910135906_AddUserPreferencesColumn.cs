4317eee024610a676aea43cf3b7f19a6352473f2
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-09-10 10:00:03 -0400

    Add EF migration for user preferences property

---
+++ src/EfRepository/Migrations/20260910135906_AddUserPreferencesColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AirWeb.EfRepository.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserPreferencesColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Preferences",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "{}");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Preferences",
+                table: "AspNetUsers");
+        }
+    }
+}
