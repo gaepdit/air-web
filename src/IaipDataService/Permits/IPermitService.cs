af9617aae95dec4fb97c32cc553041ece6f59403
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-09-11 12:11:46 -0400

    Split handling of facility ID search from other search methods

--- src/IaipDataService/Permits/IPermitService.cs
+++ src/IaipDataService/Permits/IPermitService.cs
@@ -1,4 +1,6 @@
-﻿namespace IaipDataService.Permits;
+﻿using IaipDataService.Facilities;
+
+namespace IaipDataService.Permits;
 
 public interface IPermitService
 {
@@ -6,17 +8,29 @@ public interface IPermitService
     /// Searches for permits for a facility ID or partial facility name.
     /// </summary>
     /// <param name="facilityId">A Facility ID.</param>
+    /// <param name="skip">The number of permits search results to skip (for pagination).</param>
+    /// <param name="take">The number of permits search results to take (for pagination).</param>
+    Task<IReadOnlyCollection<PermitSummary>> SearchPermitsAsync(FacilityId facilityId, int skip, int take);
+
+    /// <summary>
+    /// Searches for permits by partial facility name.
+    /// </summary>
     /// <param name="name">The facility name to search for.</param>
     /// <param name="skip">The number of permits search results to skip (for pagination).</param>
     /// <param name="take">The number of permits search results to take (for pagination).</param>
-    Task<IReadOnlyCollection<PermitSummary>> GetPermitListAsync(string? facilityId, string? name, int skip, int take);
+    Task<IReadOnlyCollection<PermitSummary>> SearchPermitsAsync(string? name, int skip, int take);
 
     /// <summary>
-    /// Counts the number of permits for a facility ID or partial facility name.
+    /// Counts the number of permits for a facility.
     /// </summary>
     /// <param name="facilityId">A Facility ID.</param>
+    Task<int> CountPermitsAsync(FacilityId facilityId);
+
+    /// <summary>
+    /// Counts the number of permits by searching by partial facility name.
+    /// </summary>
     /// <param name="name">The facility name to search for.</param>
-    Task<int> CountPermitsAsync(string? facilityId, string? name);
+    Task<int> CountPermitsAsync(string? name);
 
     /// <summary>
     /// Get a permit document.
