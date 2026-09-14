fde4e96b20197f3054355eff5b319c3ccb7bc769
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-09-14 11:38:23 -0400

    Add comments to empty EF migration methods

--- src/EfRepository/Migrations/20260910134925_UpdateEnumConfig.cs
+++ src/EfRepository/Migrations/20260910134925_UpdateEnumConfig.cs
@@ -10,13 +10,13 @@ namespace AirWeb.EfRepository.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            // Method intentionally left empty.
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            // Method intentionally left empty.
         }
     }
 }
